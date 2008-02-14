9ee6873d962ef0252b1d90f568cedb24c72a1e9f
Terrence Talbot <ttalbot@karelia.com>
2008-02-14 01:23:22 +0000

    add of LinkListElement from KTPlugins added Generate Localizable.strings to Elements I recently checked in removed KTPlugins reference from Marvel -- those plugins are no longer built and included

---
+++ Plugins/Elements/LinkListElement/LinkListInspectorController.m
@@ -0,0 +1,29 @@
+//
+//  LinkListInspectorController.m
+//  KTPlugins
+//
+//  Created by Mike on 09/05/2007.
+//  Copyright 2007 Karelia Software. All rights reserved.
+//
+
+#import "LinkListInspectorController.h"
+
+#import <NTBoxView.h>
+
+
+@implementation LinkListInspectorController
+
+- (void)awakeFromNib
+{
+	// Set up the box under the table
+	[tableButtonsBox setDrawsFrame:YES];
+	[tableButtonsBox setFill:NTBoxBevel];
+	[tableButtonsBox setBorderMask:(NTBoxLeft | NTBoxRight | NTBoxBottom)];
+	[tableButtonsBox setFrameColor:[NSColor lightGrayColor]];
+	
+	// Give the buttons their icons
+	[addLinkButton setImage:[NSImage addToTableButtonIcon]];
+	[removeLinkButton setImage:[NSImage removeFromTableButtonIcon]];
+}
+
+@end
