25560474573359da63c3e38a5f8bcdf04b59be65
Mike Abdullah <mabdullah@karelia.com>
2008-02-15 15:41:52 +0000

    BUGSID:8633 New class KTControllerProxy. This is actually a pretty simple subclass of NSObjectController that's designed to act as a proxy (dir!) for another controller.
    The main use of this will be the Inspector. In the Info.nib we now have a KTControllerProxy to represent the selected pages. You bind Inspector views to this proxy and everything gets passed along to the Site Outline's controller.

---
+++ App/KTControllerProxy.h
@@ -0,0 +1,17 @@
+//
+//  KTControllerProxy.h
+//  Marvel
+//
+//  Created by Mike on 15/02/2008.
+//  Copyright 2008 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface KTControllerProxy : NSObjectController
+{
+	id	myContentObject;
+}
+
+@end
