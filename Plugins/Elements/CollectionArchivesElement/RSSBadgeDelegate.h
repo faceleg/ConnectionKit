5118df5e288ace8ddd07c5e4c7d02ddc8ef34571
Mike Abdullah <mabdullah@karelia.com>
2008-02-28 17:05:39 +0000

    BUGSID:554 Cloned the RSS Badge project ready to turn it into the archives pagelet.

---
+++ Plugins/Elements/CollectionArchivesElement/RSSBadgeDelegate.h
@@ -0,0 +1,47 @@
+//
+//  RSSBadgeDelegate.h
+//  RSS Badge
+//
+//  Created by Mike on 20/11/2006.
+//  Copyright 2006 Karelia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <SandvoxPlugin.h>
+
+
+typedef enum {
+	RSSBadgeIconStyleNone = 0,
+	RSSBadgeIconStyleStandardOrangeSmall = 1,
+	RSSBadgeIconStyleStandardOrangeLarge = 2,
+	RSSBadgeIconStyleStandardGraySmall = 3,
+	RSSBadgeIconStyleStandardGrayLarge = 4,
+	RSSBadgeIconStyleAppleRSS = 5,
+	RSSBadgeIconStyleFlatXML = 6,
+	RSSBadgeIconStyleFlatRSS = 7,
+} RSSBadgeIconStyle;
+
+typedef enum {
+	RSSBadgeIconPositionLeft = 1,
+	RSSBadgeIconPositionRight = 2,
+} RSSBadgeIconPosition;
+
+
+@class MAImagePopUpButton;
+
+
+@interface RSSBadgeDelegate : KTAbstractPluginDelegate
+{
+	IBOutlet KTLinkSourceView	*collectionLinkSourceView;
+	IBOutlet MAImagePopUpButton	*iconTypePopupButton;
+}
+
+// IB Actions
+- (IBAction)clearCollectionLink:(id)sender;
+
+// Collection accessors
+- (BOOL)useLargeIconLayout;
+
+- (NSString *)feedIconResourcePath;
+
+@end
