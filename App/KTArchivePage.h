1d59b841cc534a2808ea9a0448f940f561c2dbfa
Mike Abdullah <mabdullah@karelia.com>
2008-02-29 13:59:35 +0000

    BUGSID:554 New class KTArchivePage ready to handle archive-specific code.

---
+++ App/KTArchivePage.h
@@ -0,0 +1,17 @@
+//
+//  KTArchivePage.h
+//  Marvel
+//
+//  Created by Mike on 29/02/2008.
+//  Copyright 2008 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "KTAbstractPage.h"
+
+
+@interface KTArchivePage : KTAbstractPage
+{
+}
+
+@end
---
+++ App/KTArchivePage.m
@@ -0,0 +1,30 @@
+//
+//  KTArchivePage.m
+//  Marvel
+//
+//  Created by Mike on 29/02/2008.
+//  Copyright 2008 Karelia Software. All rights reserved.
+//
+
+#import "KTArchivePage.h"
+
+#import "assertions.h"
+
+
+@implementation KTArchivePage
+
+/*	Stop KSExtensibleManagedObject kicking in
+ */
+- (id)valueForUndefinedKey:(NSString *)key
+{
+	OBASSERT_NOT_REACHED("");
+	return [super valueForUndefinedKey:key];
+}
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+{
+	OBASSERT_NOT_REACHED("");
+	[super setValue:value forUndefinedKey:key];
+}
+
+@end
