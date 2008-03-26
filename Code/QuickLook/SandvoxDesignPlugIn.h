fed09d00748dd468a364bb61b3a9dd11adcd8661
Mike Abdullah <mabdullah@karelia.com>
2008-03-26 23:54:33 +0000

    KSPlugin subclass that provides just enough code to locate Design plugins.

---
+++ Code/QuickLook/SandvoxDesignPlugIn.h
@@ -0,0 +1,18 @@
+//
+//  SandvoxDesignPlugIn.h
+//  SandvoxQuickLook
+//
+//  Created by Mike on 26/03/2008.
+//  Copyright 2008 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "KSPlugin.h"
+
+
+@interface SandvoxDesignPlugIn : KSPlugin
+{
+
+}
+
+@end
---
+++ Code/QuickLook/SandvoxDesignPlugIn.m
@@ -0,0 +1,34 @@
+//
+//  SandvoxDesignPlugIn.m
+//  SandvoxQuickLook
+//
+//  Created by Mike on 26/03/2008.
+//  Copyright 2008 Karelia Software. All rights reserved.
+//
+
+#import "SandvoxDesignPlugIn.h"
+
+
+@implementation SandvoxDesignPlugIn
+
++ (NSString *)pluginSubfolder
+{
+	return @"Designs";	// subfolder in App Support/APPNAME where this kind of plugin MAY reside.
+}
+
++ (NSString *)applicationPluginPath	// Designs in their own top-level plugin dir
+{
+	return [[[KSPlugin applicationBundle] bundlePath] stringByAppendingPathComponent:@"Designs"];
+}
+
+/*	Register all known plugin types.
+ */
++ (void)load
+{
+	[KSPlugin registerPluginClass:[self class] forFileExtension:@"svxDesign"];
+	[KSPlugin registerPluginClass:[KSPlugin class] forFileExtension:@"svxElement"];
+	[KSPlugin registerPluginClass:[KSPlugin class] forFileExtension:@"svxIndex"];
+	[KSPlugin registerPluginClass:[KSPlugin class] forFileExtension:@"svxDataSource"];
+}
+
+@end
