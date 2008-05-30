cf48fe1b82bb1ada1074fa3f4f9f7748810878c5
Terrence Talbot <ttalbot@karelia.com>
2008-05-30 21:50:10 +0000

    bump version to 1.5a16 bump expiry up downgrade ConnectionKit to 749

--- App/KTAppDelegate.m
+++ App/KTAppDelegate.m
@@ -94,7 +94,7 @@ IMPLEMENTATION NOTES & CAUTIONS:
 
 
 // Comment this out if we are not building an expiring demo
-#define EXPIRY_TIMESTAMP @"2008-05-30 11:59:59 -0800"
+#define EXPIRY_TIMESTAMP @"2008-06-20 11:59:59 -0800"
 
 // Enable this to get an Apple Design Awards Build, pre-licensed.  ALSO DEFINE AN EXPIRATION, DUDE!
 // (this is a non-expiring, worldwide, pro license)
