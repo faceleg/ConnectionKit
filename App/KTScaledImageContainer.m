0b4292421aa954d0fea9ccbfdfc94dbdd740d97d
Mike Abdullah <mabdullah@karelia.com>
2008-07-09 00:34:04 +0000

    BUGSID:31521 Better checking of whether an image needs rescaling upon opening a website. Gives approx. 13% performance improvement.

--- App/KTScaledImageContainer.m
+++ App/KTScaledImageContainer.m
@@ -13,6 +13,7 @@
 
 
 @interface KTScaledImageContainer (Private)
++ (BOOL)_fileNeedsGenerating:(NSDictionary *)newProperties :(NSDictionary *)oldProperties;
 - (BOOL)validateScalingSettings:(NSError **)error;
 @end
 
@@ -37,16 +38,27 @@
 		if (result)
 		{
 			KTScaledImageProperties *oldPropertiesObject = [self valueForKey:@"generatedProperties"];
-            if (oldPropertiesObject)
+            
+            
+            // Where did the media come from?
+            KTMediaFile *sourceFile = [oldPropertiesObject valueForKey:@"sourceFile"];
+            if (!sourceFile) sourceFile = [[self valueForKey:@"sourceMedia"] file];
+            
+            
+            if (sourceFile)
             {
-                KTMediaFile *sourceFile = [oldPropertiesObject valueForKey:@"sourceFile"];
-                
                 NSDictionary *newProperties = [sourceFile canonicalImagePropertiesForProperties:[self latestProperties]];
                 OBASSERT(newProperties);
-                NSDictionary *oldProperties = [oldPropertiesObject scalingProperties];
                 
-                if ([newProperties isEqualToDictionary:oldProperties]) {
-                    fileNeedsGenerating = NO;
+                NSDictionary *oldProperties = [oldPropertiesObject scalingProperties];
+                if (oldProperties)
+                {
+                    // So are the new properties different enough from the old to necessitate regeneration?
+                    fileNeedsGenerating = [[self class] _fileNeedsGenerating:newProperties :oldProperties];
+                }
+                else
+                {
+                    fileNeedsGenerating = [sourceFile propertiesRequireScaling:newProperties];
                 }
             }
 		}
@@ -62,6 +74,40 @@
 	}
 	
 	return result;
+}
+
++ (BOOL)_fileNeedsGenerating:(NSDictionary *)newProperties :(NSDictionary *)oldProperties
+{
+    if (![[newProperties objectForKey:@"scalingBehavior"] isEqual:[oldProperties objectForKey:@"scalingBehavior"]])
+    {
+        return YES;
+    }
+    
+    
+    NSString *fileType = [newProperties objectForKey:@"fileType"];
+    if (!fileType || ![fileType isEqualToString:[oldProperties objectForKey:@"fileType"]])
+    {
+        return YES;
+    }
+    
+    
+    float compression1 = [newProperties floatForKey:@"compression"];
+    float compression2 = [oldProperties floatForKey:@"compression"];
+    if (compression1 < (compression2 - 0.05) || compression1 > (compression2 + 0.05))
+    {
+        return YES;
+    }
+    
+    
+    float sharpening1 = [newProperties floatForKey:@"sharpeningFactor"];
+    float sharpening2 = [oldProperties floatForKey:@"sharpeningFactor"];
+    if (sharpening1 < (sharpening2 - 0.05) && sharpening1 > (sharpening2 + 0.05))
+    {
+        return YES;
+    }
+    
+    
+    return NO;
 }
 
 - (NSDictionary *)latestProperties
