91bac55334eef8beebfb8b9aff14a2be8f744dfc
Mike Abdullah <mabdullah@karelia.com>
2008-07-17 11:50:57 +0000

    BUGSID:31636 Some graphical media files weren't recording their dimensions properly.

--- App/KTWebViewTextBlock.m
+++ App/KTWebViewTextBlock.m
@@ -314,7 +314,16 @@
 	KTMediaFile *mediaFile = [image file];
 	if (mediaFile)
 	{			
-		result = [NSString stringWithFormat:
+		NSNumber *width = [mediaFile valueForKey:@"width"];
+        NSNumber *height = [mediaFile valueForKey:@"height"];
+        if (!width || !height ||
+            ![mediaFile validateValue:&width forKey:@"width" error:NULL] ||
+            ![mediaFile validateValue:&height forKey:@"height" error:NULL])
+        {
+            [mediaFile cacheImageDimensions];
+        }
+        
+        result = [NSString stringWithFormat:
 			@"text-align:left; text-indent:-9999px; background:url(%@) top left no-repeat; width:%ipx; height:%ipx;",
 			[[NSURL fileURLWithPath:[mediaFile currentPath]] absoluteString],
 			[mediaFile integerForKey:@"width"],
