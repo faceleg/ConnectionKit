7a1af49ca47711c957ada299de7cca9d2957c63f
Mike Abdullah <mabdullah@karelia.com>
2008-08-12 12:21:34 +0000

    BUGSID:32958 -[KTPage previousPage] was returning nil when querying the last page of a collection.

--- App/KTPage+Indexes.m
+++ App/KTPage+Indexes.m
@@ -141,7 +141,7 @@
 	
 	NSArray *siblings = [[self parent] navigablePages];
 	unsigned index = [siblings indexOfObjectIdenticalTo:self];
-	if (index > 0 && index < ([siblings count] - 1))
+	if (index > 0 && index < [siblings count])
 	{
 		result = [siblings objectAtIndex:index - 1];
 	}
