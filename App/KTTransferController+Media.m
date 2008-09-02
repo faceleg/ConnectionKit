58462a9f1cece2ee56b71814da77192fe79f8605
Mike Abdullah <mabdullah@karelia.com>
2008-09-02 23:02:06 +0000

    Moved as much media publishing back to the background thread as possible.

--- App/KTTransferController+Media.m
+++ App/KTTransferController+Media.m
@@ -17,54 +17,76 @@
 #pragma mark -
 #pragma mark Publishing
 
+- (NSSet *)mediaFileUploads { return [NSSet setWithSet:myMediaFileUploads]; }
+
+- (void)removeAllMediaFileUploads { [myMediaFileUploads removeAllObjects]; }
+
+- (void)threadedUploadMediaFile:(KTMediaFileUpload *)mediaFileUpload
+{
+	NSDictionary *publishingInfo = [self performSelectorOnMainThreadAndReturnResult:@selector(publishingInfoForMediaFile:)
+                                                                         withObject:mediaFileUpload];
+    
+    
+    if (publishingInfo)
+    {
+        // Create the directory for the file
+        NSString *uploadPath = [[self storagePath] stringByAppendingPathComponent:[publishingInfo objectForKey:@"uploadPath"]];
+        [self recursivelyCreateDirectoriesFromPath:[uploadPath stringByDeletingLastPathComponent] setPermissionsOnAllFolders:YES];
+        
+        // Upload the file
+        NSString *sourcePath = [publishingInfo objectForKey:@"sourcePath"];
+        if (sourcePath && ![sourcePath isEqualToString:@""] )
+        {
+            [self uploadFile:sourcePath toFile:uploadPath];
+        }
+        
+        // Add the file to the upload list
+        OBASSERT(mediaFileUpload);
+        [myMediaFileUploads addObject:mediaFileUpload];
+    }
+}
+
 /*	Takes a set of MediaFileUploads and publishes them
  */
 - (void)threadedUploadMediaFiles:(NSSet *)mediaFileUploads
-{
-	[self performSelectorOnMainThread:@selector(uploadMediaFiles:) withObject:mediaFileUploads waitUntilDone:YES];
-}
-
-- (void)uploadMediaFile:(KTMediaFileUpload *)mediaFileUpload
-{
-	// This MUST be called from the main thread
-	if (![NSThread isMainThread]) {
-		[NSException raise:NSObjectInaccessibleException format:@"-[KTTransferController uploadMediaFile:] is not thread-safe"];
-	}
-	
-	// Create the directory for the file
-	NSString *uploadPath = [[self storagePath] stringByAppendingPathComponent:[mediaFileUpload pathRelativeToSite]];
-	[self recursivelyCreateDirectoriesFromPath:[uploadPath stringByDeletingLastPathComponent] setPermissionsOnAllFolders:YES];
-	
-	// Upload the file
-	NSString *sourcePath = [mediaFileUpload valueForKeyPath:@"file.currentPath"];
-	if ( (nil != sourcePath) && ![sourcePath isEqualToString:@""] )
-	{
-		[self uploadFile:sourcePath toFile:uploadPath];
-	}
-	else
-	{
-		NSLog(@"upload error: no local path for %@, skipping...", [mediaFileUpload valueForKeyPath:@"file.uniqueID"]);
-	}
-	
-	// Add the file to the upload list
-    OBASSERT(mediaFileUpload);
-	[myMediaFileUploads addObject:mediaFileUpload];
-}
-
-- (void)uploadMediaFiles:(NSSet *)mediaFileUploads
 {
 	// Run through and publish each file
 	NSEnumerator *uploadsEnumerator = [mediaFileUploads objectEnumerator];
 	KTMediaFileUpload *aMediaFileUpload;
 	while (aMediaFileUpload = [uploadsEnumerator nextObject])
 	{
-		[self uploadMediaFile:aMediaFileUpload];
+		[self threadedUploadMediaFile:aMediaFileUpload];
 	}
 }
 
-- (NSSet *)mediaFileUploads { return [NSSet setWithSet:myMediaFileUploads]; }
 
-- (void)removeAllMediaFileUploads { [myMediaFileUploads removeAllObjects]; }
+/*	Support method for -threadedUploadMediaFile: that is called on the MAIN THREAD.
+ *	
+ *	Returns a dictionary with the following keys:
+ *		sourcePath			-	The path to upload from
+ *		uploadPath			-	The media's path relative to   docRoot/subFolder/
+ */
+- (NSDictionary *)publishingInfoForMediaFile:(KTMediaFileUpload *)mediaFileUpload
+{
+    // This MUST be called from the main thread
+	if (![NSThread isMainThread]) {
+		[NSException raise:NSObjectInaccessibleException format:@"-[KTTransferController uploadMediaFile:] is not thread-safe"];
+	}
+    
+    
+    NSDictionary *result = nil;
+	
+	
+    NSString *sourcePath = [mediaFileUpload valueForKeyPath:@"file.currentPath"];
+    if (sourcePath)
+    {
+        NSString *uploadPath = [mediaFileUpload pathRelativeToSite];
+        result = [NSDictionary dictionaryWithObjectsAndKeys:sourcePath, @"sourcePath", uploadPath, @"uploadPath", nil];
+    }
+    
+    
+    return result;
+}
 
 #pragma mark -
 #pragma mark Parsed Media
