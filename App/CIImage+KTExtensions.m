16426fc4c8aca9390c8b391998b89b8b14a43f68
Mike Abdullah <mabdullah@karelia.com>
2008-10-02 15:37:13 +0000

    CropToRect no longer scales up undersized images.

--- App/CIImage+KTExtensions.m
+++ App/CIImage+KTExtensions.m
@@ -19,123 +19,63 @@
 
 - (CIImage *)imageByApplyingScalingSettings:(KTImageScalingSettings *)settings opaqueEdges:(BOOL)opaqueEdges
 {
-    CIFilter *filter;
+    CIFilter *filter;       // the working filter
 	CIImage *result = self;	// the working image that we keep modifying
 	CGSize originalSize = [self extent].size;
     
-    float inW = [settings size].width;
-    float inH = [settings size].height;
-	float finalW = inW;	// initially copied from input, may be set to zero (auto)
-	float finalH = inH;
-	float dX = 0.0, dY = 0.0;		// offset of cropping from Lower Left
-	float h=finalH, w=finalW;	// actual size that image will be scaled to.
     
-	
     
-    // For some behaviors, mark the width or height as needing to be calculated
-	//
-	switch ([settings behavior])
-	{
-		case KTScaleByFactor:
-            if ([settings scaleFactor] == 1.0)
-            {
-                return self;
-            }
-            else
-            {
-                OBASSERT_NOT_REACHED("KTScaleByFactor not implemented");
-            }
-            break;
-        
-        case KTStretchToSize:
-			// keep specified dimensions as specified .... NOT FULLY IMPLEMENTED!
-            break;
+    // Calculate dimensions
+	float scaleFactor = [settings scaleFactorForImageOfSize:originalSize];
+    float aspectRatio = [settings aspectRatioForImageOfSize:originalSize];  // Will be 1.0 except for KTStretchToFit
+    
+    float finalH, finalW;
+    if ([settings behavior] == KTStretchToSize)
+    {
+        finalW = [settings size].width;
+        finalH = [settings size].height;
+    }
+    else    // Aspect ratio is 1 since we're not doing a stretch to fit
+    {
+        finalW = scaleFactor * originalSize.width;
+        finalH = scaleFactor * originalSize.height;
+    }
             
-		case KTScaleToSize:
-			if (originalSize.width / originalSize.height > finalW/finalH)
-			{
-				// wider original than will fit: "letterbox", make height automatic
-				inH = 0;
-			}
-			else
-			{
-				// taller original than wil fit: "pillarbox" so make width automatic
-				inW = 0;
-			}
-			break;
-		
-        case KTCropToSize:
-			if (originalSize.width / originalSize.height > inW/inH)
-			{
-				// wider original than will fit, use specified height and make width automatic
-				inW = 0;
-			}
-			else
-			{
-				// taller original than wil fit: use specified width and make height automatic
-				inH = 0;
-			}
-			break;
-	}
+        
+    // Scale only if needed
+	if (scaleFactor != 1.0)
+    {
+        // Before scaling, make the edges stretch to infinity so that the edges don't have any alpha
+        if (opaqueEdges)
+        {
+            filter = [CIFilter filterWithName:@"CIAffineClamp"];
+            [filter setValue:[NSAffineTransform transform] forKey:@"inputTransform"];
+            [filter setValue:result forKey:@"inputImage"];
+            result = [filter valueForKey:@"outputImage"];
+        }
+        
+        
+        // Scale
+        result = [result imageByScalingByFactor:scaleFactor aspectRatio:aspectRatio];
+        
+        
+        // Remove the infinite edge pixels if needed
+        if (opaqueEdges && [settings behavior] != KTCropToSize) // CropToSize will do the crop anyway in a sec
+        {
+            CIVector *cropRect = [CIVector vectorWithX:0.0 Y:0.0 Z:finalW W:finalH];
+            result = [self imageByCroppingToRectangle:cropRect];
+        }
+    }
 	
     
-    
-	// Now calculate any missing dimension based on original size.
-	//
-	if (0 == inW)		// specified height, calculate width
-	{
-		h = inH;
-		inW = w = roundf(inH * (originalSize.width / originalSize.height));
-		if (0 == finalW || [settings behavior] == KTScaleToSize)
-		{
-			finalW = w;
-		}
-	}
-	else if (0 == inH)	// specified width, calculate height
-	{
-		w = inW;
-		inH = h = roundf(inW * (originalSize.height / originalSize.width));
-		if (0 == finalH || [settings behavior] == KTScaleToSize)
-		{
-			finalH = h;
-		}
-	}
-	
-    
-    
-	// Before scaling, make the edges stretch to infinity so that the edges don't have any alpha
-	//
-	if (opaqueEdges)
-	{
-		filter = [CIFilter filterWithName:@"CIAffineClamp"];
-		[filter setValue:[NSAffineTransform transform] forKey:@"inputTransform"];
-		[filter setValue:result forKey:@"inputImage"];
-		result = [filter valueForKey:@"outputImage"];
-	}
-	
-	
-    // Scale
-	float xscale = w / originalSize.width;
-	float yscale = h / originalSize.height;
-    float aspectRatio = ([settings behavior] == KTStretchToSize) ? (yscale / xscale) : 1.0;
-    
-    result = [result imageByScalingByFactor:yscale aspectRatio:aspectRatio];
-    
-	
-    
-	// Crop
+	// Crop to size if requested
 	if ([settings behavior] == KTCropToSize)
 	{
-		result = [result imageByCroppingToSize:CGSizeMake(finalW, finalH)
-                                    withinRect:CGRectMake(0.0, 0.0, w, h)
+		result = [result imageByCroppingToSize:CGSizeMake([settings size].width, [settings size].height)
+                                    withinRect:CGRectMake(0.0, 0.0, finalW, finalH)
                                      alignment:[settings alignment]];
 	}
-	else
-	{
-        CIVector *cropRect = [CIVector vectorWithX:dX Y:dY Z:finalW W:finalH];
-        result = [self imageByCroppingToRectangle:cropRect];
-    }
-    
+	 
 	
 	return result;
 }
