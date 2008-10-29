b4283c61ffdf923fef2432dcd383d49653ac7e78
Mike Abdullah <mabdullah@karelia.com>
2008-10-29 19:04:45 +0000

    Twitter pagelet passes W3C validation.

--- Plugins/Elements/TwitterElement/twittercallbacktemplate.js
+++ Plugins/Elements/TwitterElement/twittercallbacktemplate.js
@@ -12,7 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-function twitterCallback_withOptions(obj, listID, linksinnewwindows, includetimestamp) {
+function twitterCallback_withOptions(obj, divid, linksinnewwindows, includetimestamp) {
 	var wwwregular = /\bwww\.\w.\w/ig;
 	var regular = /((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g;
 	var atregular = /\B@([_a-z0-9]+)/ig;
@@ -34,7 +34,7 @@ function twitterCallback_withOptions(obj, listID, linksinnewwindows, includetime
 		statusHTML += ('</li>');
 	}
 	
-	var twitterupdatelist = document.getElementById(listID);
+	var twitterupdatelist = document.createElement('ul');
 	twitterupdatelist.innerHTML = statusHTML;
 	
 	if (linksinnewwindows)
@@ -44,6 +44,9 @@ function twitterCallback_withOptions(obj, listID, linksinnewwindows, includetime
 			m[i].target = "_blank";
 		}
 	}
+	
+	var twitterupdatediv = document.getElementById(divid);
+	twitterupdatediv.appendChild(twitterupdatelist);
 }
 
 function relative_time(time_value) {
