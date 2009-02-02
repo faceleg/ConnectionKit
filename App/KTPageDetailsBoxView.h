30667fcc083c086f3180cb8dc78891bc24e4390e
Dan Wood <dwood@karelia.com>
2009-02-02 23:21:33 +0000

    starting custom subclass for better resizing

---
+++ App/KTPageDetailsBoxView.h
@@ -0,0 +1,17 @@
+//
+//  KTPageDetailsBoxView.h
+//  Marvel
+//
+//  Created by Dan Wood on 2/2/09.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "NTBoxView.h"
+
+
+@interface KTPageDetailsBoxView : NTBoxView {
+
+}
+
+@end
