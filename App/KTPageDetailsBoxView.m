3391caa55fa03332b4ce806d2e39398660d87db5
Mike Abdullah <mabdullah@karelia.com>
2009-02-20 18:27:15 +0000

    Merging 1st and 2nd February (UTC).

---
+++ App/KTPageDetailsBoxView.m
@@ -0,0 +1,26 @@
+//
+//  KTPageDetailsBoxView.m
+//  Marvel
+//
+//  Created by Dan Wood on 2/2/09.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+#import "KTPageDetailsBoxView.h"
+
+
+@implementation KTPageDetailsBoxView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+}
+
+@end
