a359b906f8d21c9a1a8c0f7363a1184534e021d6
Mike Abdullah <mabdullah@karelia.com>
2009-07-16 16:44:45 +0000

    Duplicating a page could mean the timestamp was incorrect until the document was re-opened. #43230

--- App/Elements+Pasteboard.m
+++ App/Elements+Pasteboard.m
@@ -288,6 +288,7 @@
 	
 	
 	// Wake up the page
+    [result performSelector:@selector(reloadEditableTimestamp)];    // otherwise timestamp can be incorrect #43230
 	[result awakeFromBundleAsNewlyCreatedObject:NO];
 	
 	
