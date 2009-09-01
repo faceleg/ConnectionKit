a22f478d33de54386c5452b75820670b4cda0cf3
Mike Abdullah <mabdullah@karelia.com>
2009-09-01 23:18:03 +0000

    Placeholder for SVContainerTextBlock. This is what I intend to use for more advanced lumps of text containing images etc.

---
+++ App/SVContainerTextBlock.h
@@ -0,0 +1,20 @@
+//
+//  SVContainerTextBlock.h
+//  Sandvox
+//
+//  Created by Mike on 01/09/2009.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+//  Takes a regular text block and adds the ability to embed pagelets in it.
+
+
+#import "SVBindableTextBlock.h"
+
+
+@interface SVContainerTextBlock : SVBindableTextBlock
+{
+
+}
+
+@end
---
+++ App/SVContainerTextBlock.m
@@ -0,0 +1,14 @@
+//
+//  SVContainerTextBlock.m
+//  Sandvox
+//
+//  Created by Mike on 01/09/2009.
+//  Copyright 2009 Karelia Software. All rights reserved.
+//
+
+#import "SVContainerTextBlock.h"
+
+
+@implementation SVContainerTextBlock
+
+@end
