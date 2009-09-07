aa351165425476e6d597b527484746da4678de03
Mike Abdullah <mabdullah@karelia.com>
2009-09-07 11:37:33 +0000

    This seems pretty neat, using CAConstraintLayoutManager to setup selection handle positions.

--- App/SVSelectionBorder.m
+++ App/SVSelectionBorder.m
@@ -25,7 +25,7 @@
 {
     CALayer *result = [[CALayer alloc] init];
     
-    [result setValue:[NSValue valueWithSize:NSMakeSize(6.0, 6.0)]
+    [result setValue:[NSValue valueWithSize:NSMakeSize(7.0, 7.0)]
           forKeyPath:@"bounds.size"];
     
     [result setBackgroundColor:CGColorGetConstantColor(kCGColorWhite)];
@@ -41,39 +41,68 @@
 {
     self = [super init];
     
+    [self setLayoutManager:[CAConstraintLayoutManager layoutManager]];
+    
     // Add selection handles
+    CAConstraint *minXConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidX 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMinX];
+    
+    CAConstraint *midXConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidX 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMidX];
+    
+    CAConstraint *maxXConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidX 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMaxX
+                                                                  offset:-1.0];
+    
+    CAConstraint *minYConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidY 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMinY];
+    
+    CAConstraint *midYConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidY 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMidY];
+    
+    CAConstraint *maxYConstraint = [CAConstraint constraintWithAttribute:kCAConstraintMidY 
+                                                              relativeTo:@"superlayer"
+                                                               attribute:kCAConstraintMaxY
+                                                                  offset:-1.0];
+    
+    
     _bottomLeftSelectionHandle = [self newSelectionHandle];
-    [_bottomSelectionHandle setAutoresizingMask:(kCALayerMaxXMargin | kCALayerMaxYMargin)];
+    [_bottomLeftSelectionHandle addConstraint:minXConstraint];
+    [_bottomLeftSelectionHandle addConstraint:minYConstraint];
     
     _leftSelectionHandle = [self newSelectionHandle];
-    [_leftSelectionHandle setAutoresizingMask:(kCALayerMaxXMargin |
-                                               kCALayerMinYMargin |
-                                               kCALayerMaxYMargin)];
+    [_leftSelectionHandle addConstraint:minXConstraint];
+    [_leftSelectionHandle addConstraint:midYConstraint];
     
     _topLeftSelectionHandle = [self newSelectionHandle];
-    [_topLeftSelectionHandle setAutoresizingMask:(kCALayerMaxXMargin | kCALayerMinYMargin)];
-    
+    [_topLeftSelectionHandle addConstraint:minXConstraint];
+    [_topLeftSelectionHandle addConstraint:maxYConstraint];
+
     _bottomRightSelectionHandle = [self newSelectionHandle];
-    [_bottomRightSelectionHandle setAutoresizingMask:(kCALayerMinXMargin | kCALayerMaxYMargin)];
-    
+    [_bottomRightSelectionHandle addConstraint:maxXConstraint];
+    [_bottomRightSelectionHandle addConstraint:minYConstraint];
+
     _rightSelectionHandle = [self newSelectionHandle];
-    [_rightSelectionHandle setAutoresizingMask:(kCALayerMinXMargin |
-                                                kCALayerMinYMargin |
-                                                kCALayerMaxYMargin)];
-    
+    [_rightSelectionHandle addConstraint:maxXConstraint];
+    [_rightSelectionHandle addConstraint:midYConstraint];
+
     _topRightSelectionHandle = [self newSelectionHandle];
-    [_topRightSelectionHandle setAutoresizingMask:(kCALayerMinXMargin | kCALayerMinYMargin)];
-    
+    [_topRightSelectionHandle addConstraint:maxXConstraint];
+    [_topRightSelectionHandle addConstraint:maxYConstraint];
+
     _bottomSelectionHandle = [self newSelectionHandle];
-    [_bottomSelectionHandle setAutoresizingMask:(kCALayerMaxYMargin |
-                                                 kCALayerMinXMargin |
-                                                 kCALayerMaxXMargin)];
-    
+    [_bottomSelectionHandle addConstraint:midXConstraint];
+    [_bottomSelectionHandle addConstraint:minYConstraint];
+
     _topSelectionHandle = [self newSelectionHandle];
-    [_topSelectionHandle setAutoresizingMask:(kCALayerMinYMargin |
-                                              kCALayerMinXMargin |
-                                              kCALayerMaxXMargin)];
-    
+    [_topSelectionHandle addConstraint:midXConstraint];
+    [_topSelectionHandle addConstraint:maxYConstraint];
+
     
     // Add our border
     [self setBorderColor:[[NSColor selectedControlColor] CGColor]];
