d9b0cbbb508481f88d0bd68b6e43d0b374780a4c
Mike Abdullah <mabdullah@karelia.com>
2009-09-23 17:37:43 +0000

    Using a plain method, not @property in protocol declaration to stop build warnings.

--- App/SVEditingOverlayItem.h
+++ App/SVEditingOverlayItem.h
@@ -13,6 +13,6 @@
 
 /*  The item's placement within the document view. i.e. 0,0 will place you at the top-left of the doc. 10,10 will set it 10 pixels to the right, and 10 pixels down the document.
  */
-@property(nonatomic, readonly) DOMElement *DOMElement;
+- (DOMElement *)DOMElement;
 
 @end
