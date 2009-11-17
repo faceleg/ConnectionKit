b651655993bd2b7d66a93b3367cf4b10a4433303
Dan Wood <dwood@karelia.com>
2009-11-17 04:55:09 +0000

    checking in initial attempt at a subscribe form for 1.6.x, let's see if I can get something running quickly

---
+++ Plugins/Elements/SubscribeElement/SubscribeElementField.h
@@ -0,0 +1,107 @@
+//
+//  SubscribeElementField.h
+//  SubscribeElement
+//
+//  Copyright 2007-2009 Karelia Software. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  *  Redistribution of source code must retain the above copyright notice,
+//     this list of conditions and the follow disclaimer.
+//
+//  *  Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other material provided with the distribution.
+//
+//  *  Neither the name of Karelia Software nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS-IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUR OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Community Note: This code is distrubuted under a modified BSD License.
+//  We encourage you to share your Sandvox Plugins similarly.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+typedef enum
+{
+	SubscribeElementTextFieldField = 1,
+	SubscribeElementTextAreaField = 2,
+	SubscribeElementCheckBoxField = 3,
+	SubscribeElementPopupButtonField = 4,
+	SubscribeElementRadioButtonsField = 5,
+	SubscribeElementSubmitButton = 6,
+	SubscribeElementHiddenField = 0,
+} SubscribeElementFieldType;
+
+
+@class SubscribeElementDelegate;
+
+
+@interface SubscribeElementField : NSObject <NSCopying>
+{
+	SubscribeElementDelegate	*myOwner;
+	
+	NSString				*myIdentifier;
+	SubscribeElementFieldType	myType;
+	NSString				*myLabel;
+	NSString				*myDefaultString;
+	NSString				*myCheckBoxLabel;
+	BOOL					myCheckBoxIsSelected;
+	NSArray					*myVisitorChoices;
+}
+
+// Init
+- (id)initWithIdentifier:(NSString *)identifier;
+- (id)initWithDictionary:(NSDictionary *)dictionary;
+
+// Owner
+- (SubscribeElementDelegate *)owner;
+- (void)setOwner:(SubscribeElementDelegate *)owner;
+
+// Accessors
+- (NSString *)identifier;
+
+- (SubscribeElementFieldType)type;
+- (void)setType:(SubscribeElementFieldType)type;
+
+- (NSString *)label;
+- (void)setLabel:(NSString *)label;
+- (NSString *)labelWithLocalizedColonSuffix;
+
+- (NSString *)defaultString;
+- (void)setDefaultString:(NSString *)defaultString;
+
+- (NSString *)checkBoxLabel;
+- (void)setCheckBoxLabel:(NSString *)label;
+
+- (BOOL)checkBoxIsSelected;
+- (void)setCheckBoxIsSelected:(BOOL)selected;
+
+- (NSArray *)visitorChoices;
+- (void)setVisitorChoices:(NSArray *)choices;
+
+// UI
+- (BOOL)shouldDrawLockIcon;
+
+// Storage
+- (NSDictionary *)dictionaryRepresentation;
+
+// HTML
+- (NSString *)inputName;
+
+@end
---
+++ Plugins/Elements/SubscribeElement/SubscribeElementFieldsArrayController.m
@@ -0,0 +1,117 @@
+//
+//  SubscribeElementFieldsArrayController.m
+//  SubscribeElement
+//
+//  Copyright 2007-2009 Karelia Software. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  *  Redistribution of source code must retain the above copyright notice,
+//     this list of conditions and the follow disclaimer.
+//
+//  *  Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other material provided with the distribution.
+//
+//  *  Neither the name of Karelia Software nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS-IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUR OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Community Note: This code is distrubuted under a modified BSD License.
+//  We encourage you to share your Sandvox Plugins similarly.
+//
+
+#import "SubscribeElementFieldsArrayController.h"
+#import "SubscribeElementField.h"
+#import "SubscribeElementFieldCell.h"
+
+
+@implementation SubscribeElementFieldsArrayController
+
+/*	Returned object should have retain count of 1
+ */
+//LocalizedStringInThisBundle(@"New field", "The default label for new fields")
+- (id)newObject
+{
+	SubscribeElementField *newField = [[SubscribeElementField alloc] initWithIdentifier:@"other"];
+	
+	[newField setType:SubscribeElementTextFieldField];
+	
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	NSString *language = [[pluginDelegate page] valueForKeyPath:@"master.language"];
+	[newField setLabel:[bundle localizedStringForString:@"New field" language:language]];
+	
+	return newField;
+}
+
+/*	Ensure newly inserted objects are selected.
+ */
+- (void)insertObject:(id)object atArrangedObjectIndex:(unsigned int)index
+{
+	[super insertObject:object atArrangedObjectIndex:index];
+	[self setSelectionIndex:index];
+}	
+
+/*	When the user adds a new field:
+ *	If the Message field is the last in the list, add this new field just before it
+ */
+- (void)add:(id)sender
+{
+	NSArray *fields = [self arrangedObjects];
+	if ([[[fields lastObject] identifier] isEqualToString:@"send"])
+	{
+		int subtract = 1;
+		if ([[[fields objectAtIndex:[fields count]-2] identifier] isEqualToString:@"message"])
+		{
+			subtract++;
+		}
+		SubscribeElementField *newField = [self newObject];
+		[self insertObject:newField atArrangedObjectIndex:([fields count] - subtract)];
+		[newField release];
+	}
+	else
+	{
+		[super add:sender];
+	}
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"AddedMessageField" object:self];
+}
+
+/*	If removing the last object in the array, select the new last object
+ */	
+- (void)removeObjectAtArrangedObjectIndex:(unsigned int)index
+{
+	[super removeObjectAtArrangedObjectIndex:index];
+	
+	unsigned count = [[self arrangedObjects] count];
+	if (index >= count) {
+		[self setSelectionIndex:(count - 1)];
+	}
+}
+
+#pragma mark -
+#pragma mark Drawing
+
+/*	Set the -locked property of cells before they draw
+ */
+- (void)tableView:(NSTableView *)aTableView willDisplayCell:(id)aCell forTableColumn:(NSTableColumn *)aTableColumn row:(int)rowIndex
+{
+	if (![aCell isKindOfClass:[SubscribeElementFieldCell class]]) return;
+	
+	SubscribeElementField *field = [[self arrangedObjects] objectAtIndex:rowIndex];
+	[aCell setLocked:[field shouldDrawLockIcon]];
+}
+
+@end
