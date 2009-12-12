47a7031ea87520c3f88efc5b36a986bd82c214bc
Mike Abdullah <mabdullah@karelia.com>
2009-12-12 21:59:04 +0000

    Turning text block ID assertion back off as it's unwanted once you start editing.

--- App/SVHTMLContext.m
+++ App/SVHTMLContext.m
@@ -131,9 +131,6 @@
 {
     OBPRECONDITION(_textBlocks);
     
-    // There REALLY shouldn't be 2 blocks with the same ID.
-    OBASSERT(![[_textBlocks valueForKey:@"DOMNodeID"] containsObject:[textBlock DOMNodeID]]);
-    
     [_textBlocks addObject:textBlock];
 }
 
