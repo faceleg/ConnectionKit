a1415a34f5012c22c078eebeb2f5d248124a5eac
Mike Abdullah <mabdullah@karelia.com>
2010-01-04 15:39:06 +0000

    Documenting how to use page methods. #59894

--- App/SVElementPlugIn.h
+++ App/SVElementPlugIn.h
@@ -58,6 +58,13 @@
 - (id)serializedValueForKey:(NSString *)key;
 - (void)setSerializedValue:(id)serializedValue forKey:(NSString *)key;
 
+/*  FAQ:    How do I reference a page from a plug-in?
+ *
+ *      Once you've gotten hold of an SVPage object, it's fine to hold it in memory like any other object; just shove it in an instance variable and retain it. You should then also observe SVPageWillBeDeletedNotification and use it discard your reference to the page, as it will become invalid ater that.
+ *      To persist your reference to the page, override -serializedValueForKey: to use the page's -identifier property. Likewise, override -setSerializedValue:forKey: to take the serialized ID string and convert it back into a SVPage using -pageWithIdentifier:
+ *      All of these methods are documented in SVPageProtocol.h
+ */
+
 
 #pragma mark The Wider World
 
