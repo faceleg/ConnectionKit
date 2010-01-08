d90a4a6930a8693f43ddc1919a02e1b413ea46d4
Mike Abdullah <mabdullah@karelia.com>
2010-01-08 00:12:59 +0000

    Sidebar HTML generation records KVO dependencies, so reordering pagelets updates the view. #59554

--- App/SVSidebar.m
+++ App/SVSidebar.m
@@ -33,6 +33,16 @@
 - (void)writePageletsHTML;
 {
     NSArray *pagelets = [SVPagelet arrayBySortingPagelets:[self pagelets]];
+    
+    // Kinda hacky, have to record a series of dependencies to handle sorting. #59554
+    SVHTMLContext *context = [SVHTMLContext currentContext];
+    [context addDependencyOnObject:self keyPath:@"pagelets"];
+    for (SVPagelet *aPagelet in pagelets)
+    {
+        [context addDependencyOnObject:aPagelet keyPath:@"sortKey"];
+    }
+    
+    // Write HTML
     [SVContentObject writeContentObjects:pagelets];
 }
 
