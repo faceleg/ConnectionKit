7acaeb54567227c4b7e7a5b08b8b0c6ead7f6385
Mike Abdullah <mabdullah@karelia.com>
2010-01-10 23:55:11 +0000

    Link Inspector doesn't need to track WebView selection.

--- App/SVLinkInspector.m
+++ App/SVLinkInspector.m
@@ -17,39 +17,10 @@
 
 @implementation SVLinkInspector
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(webViewDidChangeSelection:) name:WebViewDidChangeSelectionNotification object:nil];
-    
-    return self;
-}
-
-- (void)dealloc
-{
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:WebViewDidChangeSelectionNotification object:nil];
-    
-    [super dealloc];
-}
-
 #pragma mark Inspection
 
 @synthesize inspectedWindow = _inspectedWindow;
 @synthesize inspectedLink = _inspectedLink;
-
-- (void)webViewDidChangeSelection:(NSNotification *)notification
-{
-    // Ignore any webview that isn't the one we're interested in
-    WebView *webView = [[[[[self inspectedWindow] windowController] webContentAreaController] webEditorViewController] webView];
-    if ([notification object] != webView) return;
-    
-    
-    // If there is link selected, we can find it by searching out from the selecte DOM range
-    DOMRange *selection = [webView selectedDOMRange];
-    DOMHTMLAnchorElement *link = [selection editableAnchorElement];
-    [self setInspectedLink:link];
-}
 
 #pragma mark Link View
 
