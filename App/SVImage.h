9069fe96083b59469f7d2e51ef0c699b20d0b742
Mike Abdullah <mabdullah@karelia.com>
2010-01-27 10:31:43 +0000

    Class for images. #62660

---
+++ App/SVImage.h
@@ -0,0 +1,23 @@
+//
+//  SVImage.h
+//  Sandvox
+//
+//  Created by Mike on 27/01/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+#import "SVGraphic.h"
+
+@class SVMediaRecord;
+@class SVStringAttribute;
+
+@interface SVImage : SVGraphic
+
+@property (nonatomic, retain) SVMediaRecord *media;
+@property (nonatomic, retain) SVStringAttribute *inlineGraphic;
+
+@end
+
+
+
---
+++ App/SVImage.m
@@ -0,0 +1,19 @@
+// 
+//  SVImage.m
+//  Sandvox
+//
+//  Created by Mike on 27/01/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "SVImage.h"
+
+#import "SVMediaRecord.h"
+#import "SVStringAttribute.h"
+
+@implementation SVImage 
+
+@dynamic media;
+@dynamic inlineGraphic;
+
+@end
