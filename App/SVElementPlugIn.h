2c52c080d77b8210e108f48d949c7912ba1b2bc2
Mike Abdullah <mabdullah@karelia.com>
2010-02-03 15:18:52 +0000

    typo

--- App/SVElementPlugIn.h
+++ App/SVElementPlugIn.h
@@ -62,7 +62,7 @@
  *
  *      Once you've gotten hold of an SVPage object, it's fine to hold it in memory like any other object; just shove it in an instance variable and retain it. You should then also observe SVPageWillBeDeletedNotification and use it discard your reference to the page, as it will become invalid ater that.
  *      To persist your reference to the page, override -serializedValueForKey: to use the page's -identifier property. Likewise, override -setSerializedValue:forKey: to take the serialized ID string and convert it back into a SVPage using -pageWithIdentifier:
- *      All of these methods are documented in SVPageProtocol.h6
+ *      All of these methods are documented in SVPageProtocol.h
  */
 
 
