dbefd12432d0b9c58345681e7e448fcad4451643
Mike Abdullah <mabdullah@karelia.com>
2010-02-07 22:59:21 +0000

    <i> and <b> elements get converted to <em> and <strong>. #50230

--- App/SVParagraphHTMLContext.m
+++ App/SVParagraphHTMLContext.m
@@ -29,18 +29,36 @@
 {
     DOMNode *result = element;
     
+    
     // Remove any tags not allowed
+    NSString *tagName = [element tagName];
     if (![[self class] isTagAllowed:[element tagName]])
     {
-        // Figure out the preferred next node
-        result = [element firstChild];
-        if (!result) result = [element nextSibling];
-        
-        // Remove non-whitelisted element
-        [element unlink];
-        
-        // Check the new node is OK to write
-        result = [result willWriteHTMLToContext:self];
+        // Convert a bold or italic tag to <strong> or <em>
+        if ([tagName isEqualToString:@"B"])
+        {
+            result = [[element parentNode] replaceChildNode:element
+                                     withElementWithTagName:@"STRONG"
+                                               moveChildren:YES];
+        }
+        else if ([tagName isEqualToString:@"I"])
+        {
+            result = [[element parentNode] replaceChildNode:element
+                                     withElementWithTagName:@"EM"
+                                               moveChildren:YES];
+        }
+        else
+        {
+            // Figure out the preferred next node
+            result = [element firstChild];
+            if (!result) result = [element nextSibling];
+            
+            // Remove non-whitelisted element
+            [element unlink];
+            
+            // Check the new node is OK to write
+            result = [result willWriteHTMLToContext:self];
+        }
     }
     
     return result;
