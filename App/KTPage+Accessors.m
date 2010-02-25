5db752556e3fa831bdcc9ed77d23151f863765de
Mike Abdullah <mabdullah@karelia.com>
2010-02-25 13:22:04 +0000

    warning fix.

--- App/KTPage+Accessors.m
+++ App/KTPage+Accessors.m
@@ -18,6 +18,7 @@
 #import "NSArray+Karelia.h"
 #import "NSDocumentController+KTExtensions.h"
 #import "NSManagedObject+KTExtensions.h"
+#import "NSManagedObjectContext+Karelia.h"
 #import "NSObject+Karelia.h"
 #import "NSData+Karelia.h"
 #import "NSString+Karelia.h"
