c8866ab736802758d41d60a50bf78fc8ba40e733
Mike Abdullah <mabdullah@karelia.com>
2010-03-01 15:35:59 +0000

    Ignoring newline characters in text fields.

--- App/SVFieldEditorHTMLStream.m
+++ App/SVFieldEditorHTMLStream.m
@@ -310,6 +310,9 @@
 // Comments have no place in text fields! Yes, they get left in the DOM until it's replaced, but you can't see them, so no harm done
 - (void)writeComment:(NSString *)comment; { }
 
+// Newlines have no place in text fields either.
+- (void)writeNewline { }
+
 #pragma mark Primitive Writing
 
 - (void)writeString:(NSString *)string
