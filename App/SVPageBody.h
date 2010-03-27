80942029f9199f8f5ab3b6f90e95ba7a8711dd1b
Mike Abdullah <mabdullah@karelia.com>
2010-03-27 22:02:40 +0000

    Dedicated SVPageBody class.

---
+++ App/SVPageBody.h
@@ -0,0 +1,21 @@
+//
+//  SVPageBody.h
+//  Sandvox
+//
+//  Created by Mike on 27/03/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "SVRichText.h"
+
+
+@class KTPage;
+
+@interface SVPageBody : SVRichText
+
+@property (nonatomic, retain, readonly) KTPage *page;
+
+@end
+
+
+
---
+++ App/SVPageBody.m
@@ -0,0 +1,17 @@
+// 
+//  SVPageBody.m
+//  Sandvox
+//
+//  Created by Mike on 27/03/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "SVPageBody.h"
+
+#import "KTPage.h"
+
+@implementation SVPageBody 
+
+@dynamic page;
+
+@end
