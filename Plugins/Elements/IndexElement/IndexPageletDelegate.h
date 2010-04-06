16399c0d7041cc402cbc04f11e3acb11c580744a
Mike Abdullah <mabdullah@karelia.com>
2010-04-06 18:30:45 +0000

    Marking Collection Index plug-in as an index.

--- Plugins/Elements/IndexElement/IndexPageletDelegate.h
+++ Plugins/Elements/IndexElement/IndexPageletDelegate.h
@@ -39,7 +39,7 @@
 
 @class KTLinkSourceView, IndexPageletLinkSourceView;
 
-@interface IndexPageletDelegate : SVPageletPlugIn
+@interface IndexPageletDelegate : SVPageletPlugIn <SVIndexPlugIn>
 {
 	IBOutlet KTLinkSourceView *oLinkView;
 }
