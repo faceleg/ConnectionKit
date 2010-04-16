6f1d27d9eae09b8422bb801325285aa90f569b35
Mike Abdullah <mabdullah@karelia.com>
2010-04-16 12:01:21 +0000

    Removing code left from old way of handling extra resources.

--- App/KTPage+Web.m
+++ App/KTPage+Web.m
@@ -111,17 +111,6 @@
     [context close];
     
     
-	// Ask the delegate for any extra resource files that the parser didn't catch
-    NSMutableSet *resources = [[NSMutableSet alloc] init];
-    NSString *aResourcePath;
-    for (aResourcePath in resources)
-    {
-        [publishingEngine addResourceFile:[NSURL fileURLWithPath:aResourcePath]];
-    }
-    
-    [resources release];
-	
-	
 	// Generate and publish RSS feed if needed
 	if ([self collectionSyndicate] && [self collectionCanSyndicate])
 	{
