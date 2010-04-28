2e350c6fcf57bae238ed78856c85c2765a8101d1
Terrence Talbot <ttalbot@karelia.com>
2010-04-28 17:35:08 +0000

    add new xib and view controller

---
+++ Plugins/Elements/FeedElement/FeedInspector.h
@@ -0,0 +1,17 @@
+//
+//  FeedInspector.h
+//  FeedElement
+//
+//  Created by Terrence Talbot on 4/27/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SandvoxPlugin.h"
+
+@interface FeedInspector : SVInspectorViewController
+{
+
+}
+
+@end
---
+++ Plugins/Elements/FeedElement/FeedInspector.m
@@ -0,0 +1,14 @@
+//
+//  FeedInspector.m
+//  FeedElement
+//
+//  Created by Terrence Talbot on 4/27/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "FeedInspector.h"
+
+
+@implementation FeedInspector
+
+@end
