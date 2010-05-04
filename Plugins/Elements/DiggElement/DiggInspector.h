e55accce14b55dc262d3d137b68ca5d502053cc7
Terrence Talbot <ttalbot@karelia.com>
2010-05-04 22:30:00 +0000

    add inspector class

---
+++ Plugins/Elements/DiggElement/DiggInspector.h
@@ -0,0 +1,16 @@
+//
+//  DiggInspector.h
+//  DiggElement
+//
+//  Created by Terrence Talbot on 5/4/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DiggInspector : NSObject {
+
+}
+
+@end
---
+++ Plugins/Elements/DiggElement/DiggInspector.m
@@ -0,0 +1,14 @@
+//
+//  DiggInspector.m
+//  DiggElement
+//
+//  Created by Terrence Talbot on 5/4/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "DiggInspector.h"
+
+
+@implementation DiggInspector
+
+@end
