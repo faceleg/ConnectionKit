7655c6a29854b2b183f245e92436b1b8c1f44113
Terrence Talbot <ttalbot@karelia.com>
2010-05-17 22:12:31 +0000

    converting SiteMap to SVPlugin

---
+++ Plugins/Elements/SiteMapElement/SiteMapPlugIn.h
@@ -0,0 +1,16 @@
+//
+//  SiteMapPlugIn.h
+//  SiteMapElement
+//
+//  Created by Terrence Talbot on 5/17/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SiteMapPlugIn : NSObject {
+
+}
+
+@end
---
+++ Plugins/Elements/SiteMapElement/SiteMapPlugIn.m
@@ -0,0 +1,14 @@
+//
+//  SiteMapPlugIn.m
+//  SiteMapElement
+//
+//  Created by Terrence Talbot on 5/17/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "SiteMapPlugIn.h"
+
+
+@implementation SiteMapPlugIn
+
+@end
