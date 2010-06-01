b34271736de1cbecf2a7e2152de02ee3f6fdb0dc
Terrence Talbot <ttalbot@karelia.com>
2010-06-01 04:37:57 +0000

    a reasonable stab at a sitemap, finally

--- Plugins/Elements/SiteMapElement/SiteMapPlugIn.m
+++ Plugins/Elements/SiteMapElement/SiteMapPlugIn.m
@@ -40,24 +40,6 @@
 #import "SiteMapPlugIn.h"
 
 
-@interface NSMutableString (indentation)
-- (void)appendIndent:(int)anIndent;
-@end
-
-
-@implementation NSMutableString (indentation)
-
-- (void)appendIndent:(int)anIndent
-{
-	int i;
-	for (i = 0 ; i < anIndent ; i++ )
-	{
-		[self appendString:@"\t"];
-	}
-}
-@end
-
-
 @implementation SiteMapPlugIn
 
 #pragma mark -
@@ -85,301 +67,174 @@
     self.compact = NO;
     self.sections = NO;
     self.showHome = YES;
-    self.showSiteMap = YES;
+    self.showSiteMap = YES; //FIXME: what is this property for?
 }
 
 
 #pragma mark -
 #pragma mark HTML Generation
 
-//- (void)writeMapOfPage:(id <SVPage>)aPage
-//          wantsCompact:(BOOL)wantsCompact
-//          isTopSection:(BOOL)isTopSection
-//{	
-//    if ( [aPage includeInSiteMaps] )
-//	{
-//        
-//        id <SVPlugInContext>context = [SVPageletPlugIn currentContext];
-//        
-//        
-//		// Fetch all children suitable for inclusion in the sitemap
-//        //        static NSPredicate *includeInSiteMapPredicate;
-//        //        if (!includeInSiteMapPredicate) {
-//        //            includeInSiteMapPredicate = [[NSPredicate predicateWithFormat:@"excludedFromSiteMap == 0"] retain];
-//        //        }
-//        //        
-//        //        NSArray *children = [[aPage sortedChildren] filteredArrayUsingPredicate:includeInSiteMapPredicate];
-//        
-//        NSArray *childPages = [aPage childPages];
-//		NSMutableArray *children = [NSMutableArray arrayWithCapacity:childPages.count];
-//        for ( id<SVPage> childPage in childPages )
-//        {
-//            if ( [childPage includeInSiteMaps] ) [children addObject:childPage];
-//        }
-//        
-//        //[string appendIndent:anIndent];
-//        [[context HTMLWriter] increaseIndentationLevel];
-//        
-//		if (isTopSection)
-//		{
-//			//[string appendString:@"<h3>"];
-//            [[context HTMLWriter] startElement:@"h3" attributes:nil];
-//		}
-//		else
-//		{
-//			//[string appendString:@"<li>"];
-//            [[context HTMLWriter] startElement:@"li" attributes:nil];
-//            [[context HTMLWriter] endElement];
-//		}
-//		
-//		if (aPage == thisPage)	// not likely but maybe possible
-//		{
-//			NSString *title = [aPage title];	/// HTML was leaving extra formatting; we want it less dramatic
-//            if (title)
-//            {
-//                //[string appendString:title];
-//                [[context HTMLWriter] writeText:title];
-//            }
-//		}
-//		else
-//		{
-////			/// special case for LinkPage, add target=_BLANK if LinkPage and newWindowLink
-////			if ( [[[aPage delegate] class] isEqual:[NSClassFromString(@"LinkPageDelegate") class]]
-////				 && (1 == [(NSNumber *)[aPage valueForKey:@"linkType"] intValue]) ) // 1 is newWindowLink
-////			{
-////				[string appendFormat:@"<a target=\"_blank\" href=\"%@\">%@</a>", [aPage pathRelativeTo:thisPage], [[aPage titleText] stringByEscapingHTMLEntities]];
-////			}
-////			else
-////			{
-//			//NSString *href = [[aPage URL] stringRelativeToURL:[thisPage URL]];
-//            NSString *href = [context relativeURLStringOfPage:aPage];
-//            
-//            OBASSERT(href);
-//            NSString *title = [aPage title];
-//            
-//			if (title)
-//			{
-//				//[string appendFormat:@"<a href=\"%@\">%@</a>", href, title];
-//                [[context HTMLWriter] startAnchorElementWithHref:href 
-//                                                           title:title
-//                                                          target:nil
-//                                                             rel:nil];
-//                [[context HTMLWriter] writeText:title                                           ];
-//                [[context HTMLWriter] endElement];
-//                
-//			}
-//		}
-//		
-//		if (isTopSection)
-//		{
-//			//[string appendString:@"</h3>\n"];
-//            [[context HTMLWriter] endElement];
-//		}
-//		else
-//		{
-//			// There are children -- so start them on a new line.  Otherwise we'll close <ul> below without a newline
-//			if ([children count])
-//			{
-//				//[string appendString:@"\n"];
-//			}
-//			
-//		}
-//		
-//		if ([children count])
-//		{
-//			// If we want compact, go through and look to see if any of the children have children --
-//			// if so, we can't be compact at this level.
-//			BOOL doCompact = NO;
-//			
-//			if (wantsCompact)
-//			{
-//				BOOL canBeCompact = NO;
-//                //				NSEnumerator *theEnum = [children objectEnumerator];
-//                //				id<SVPage> *aChildPage;
-//                //                
-//                //				while (aChildPage = [theEnum nextObject])
-//                //				{
-//                //					if (![aChildPage hasChildren])
-//                //					{
-//                //						canBeCompact = YES;
-//                //						break;
-//                //					}
-//                //				}
-//                
-//                for ( id<SVPage> childPage in children )
-//                {
-//                    if ( ![[childPage children] count] )
-//                    {
-//                        canBeCompact = YES;
-//                    }
-//                }
-//				doCompact = canBeCompact;	// we wanted compact, set whether to do it here
-//			}
-//			
-//			if (doCompact)	// immediately show children inline, without a recursion
-//			{
-//				NSEnumerator *theEnum = [children objectEnumerator];
-//				KTPage *aChildPage;
-//				BOOL firstPass = YES;
-//                
-//				//[string appendIndent:anIndent+1];
-//                [[context HTMLWriter] increaseIndentationLevel];
-//                
-//				[string appendString:@"<ul><li>\n"];
-//				while (aChildPage = [theEnum nextObject])
-//				{
-//                    [string appendIndent:anIndent+2];
-//                    if (firstPass)
-//                    {
-//                        firstPass = NO;
-//                    }
-//                    else
-//                    {
-//                        [string appendString:@"&middot; "];
-//                    }
-//                    if (aChildPage == thisPage)	// not likely but maybe possible
-//                    {
-//                        NSString *title = [[aChildPage titleString] stringByEscapingHTMLEntities];
-//                        OBASSERT([title lowercaseString]);
-//                        [string appendFormat:@"%@\n", title];
-//                    }
-//                    else
-//                    {
-//                        NSString *path = [[aChildPage URL] stringRelativeToURL:[thisPage URL]];
-//                        NSString *title = [[aChildPage titleString] stringByEscapingHTMLEntities];
-//                        if (path && title) [string appendFormat:@"<a href=\"%@\">%@</a>\n", path, title];
-//                    }
-//                    // need separator?	
-//				}
-//				[string appendIndent:anIndent+1];
-//				[string appendString:@"</li></ul>\n"];
-//			}
-//			else	// non-compact way -- a list
-//			{
-//				[string appendIndent:anIndent+1];
-//				[string appendString:@"<ul>\n"];
-//				NSEnumerator *theEnum = [children objectEnumerator];
-//				KTPage *aChildPage;
-//				
-//				while (nil != (aChildPage = [theEnum nextObject]) )
-//				{
-//					[self appendMapOfPage:aChildPage relativeToPage:thisPage toBuffer:string wantsCompact:wantsCompact topSection:NO indent:anIndent+2];
-//				}
-//				[string appendIndent:anIndent+1];
-//				[string appendString:@"</ul>\n"];
-//			}
-//		}
-//		
-//		// Close list item for this guy
-//		if (!isTopSection)
-//		{
-//			if ([children count])		// if there were children, then we close list on new line
-//			{
-//				[string appendIndent:anIndent];
-//			}
-//			[string appendString:@"</li>\n"];
-//		}
-//	}
-//}
+- (BOOL)canCompactChildren:(NSArray *)children
+{
+    // if any children have children, return NO
+    for ( id<SVPage> page in children )
+    {
+        if ( [[page childPages] count] )
+        {
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
+- (void)writeLinkOfPage:(id<SVPage>)aPage
+         relativeToPage:(id<SVPage>)thisPage
+              toContext:(id<SVPlugInContext>)context
+{
+    NSString *title = ([aPage title] ? [aPage title] : @"");
+    if ( [aPage isEqual:thisPage] ) // not likely but maybe possible
+    {
+        // just emit title
+        [[context HTMLWriter] writeText:title];
+    }
+    else
+    {
+        //FIXME: special case for LinkPage, add target=_BLANK if LinkPage and newWindowLink ???
+        // emit href + title
+        NSString *path = [context relativeURLStringOfPage:aPage];
+        if (!path) path = @"";  // Happens for a site with no -siteURL set yet
+        [[context HTMLWriter] startAnchorElementWithHref:path 
+                                                   title:title
+                                                  target:nil
+                                                     rel:nil];
+        [[context HTMLWriter] writeText:[thisPage title]];
+        [[context HTMLWriter] endElement];            
+    }
+}
+
+- (void)writeMapOfPage:(id<SVPage>)aPage
+        relativeToPage:(id<SVPage>)thisPage
+             toContext:(id<SVPlugInContext>)context
+             asSection:(BOOL)asSection
+          wantsCompact:(BOOL)wantsCompact
+{	
+    if ( [aPage includeInSiteMaps] ) // we must check this since we're recursive
+	{
+        // observe observable keypaths for aPage
+        id<NSFastEnumeration> keyPaths = [aPage automaticRearrangementKeyPaths];
+        for ( NSString *keyPath in keyPaths )
+        {
+            //FIXME: 75490: replace NOT watching title of thisPage with a DOM controller
+            if ( [aPage isEqual:thisPage] && [keyPath isEqualToString:@"title"] ) continue;
+            [(SVHTMLContext *)context addDependencyOnObject:aPage keyPath:keyPath];
+        }
+        
+        // figure out what children, if any, should be included        
+		NSMutableArray *children = [NSMutableArray array];
+        for ( id<SVPage> childPage in [aPage childPages] )
+        {
+            if ( [childPage includeInSiteMaps] ) [children addObject:childPage];
+        }
+            
+        // if asSection emit <h3>, else emit <li>
+        [[context HTMLWriter] startElement:(asSection ? @"h3" : @"li") attributes:nil];
+        
+        // process aPage
+        [self writeLinkOfPage:aPage relativeToPage:thisPage toContext:context];
+        
+        // close h3
+		if ( asSection ) [[context HTMLWriter] endElement];
+		
+        // process children
+		if ( [children count] )
+		{
+            if ( wantsCompact && [self canCompactChildren:children] )
+            {
+                // show children inline , no recursion
+                [[context HTMLWriter] startElement:@"ul" attributes:nil];
+                [[context HTMLWriter] startElement:@"li" attributes:nil];
+                
+                BOOL firstChild = YES;
+                for ( id<SVPage> child in children )
+                {
+                    [self writeLinkOfPage:child relativeToPage:thisPage toContext:context];
+                    // on the 2nd pass, emit \n&middot;
+                    if ( !firstChild ) [[context HTMLWriter] writeText:@"\n&middot;"];
+                    firstChild = NO;
+                }
+                
+                [[context HTMLWriter] endElement]; // </li>
+                [[context HTMLWriter] endElement]; // </ul>
+            }
+            else
+            {
+                // a simple list of children, processed recursively
+                [[context HTMLWriter] startElement:@"ul" attributes:nil];
+
+                for ( id<SVPage> child in children )
+                {
+                    [self writeMapOfPage:child 
+                          relativeToPage:thisPage 
+                               toContext:context
+                               asSection:NO // children can't be sections
+                            wantsCompact:wantsCompact];
+                }
+                
+                [[context HTMLWriter] endElement]; // </ul>
+            }
+        }
+            
+        // close li
+        if ( !asSection ) [[context HTMLWriter] endElement];
+    }
+}
 
 - (void)writeInnerHTML:(id <SVPlugInContext>)context
 {
-    // ask each page for its link and write its link
-
 	id<SVPage> thisPage = (id<SVPage>)[context page];
+    OBASSERT(thisPage);
 	id<SVPage> rootPage = [thisPage rootPage];
-    NSArray *childPages = [rootPage childPages];
+    OBASSERT(rootPage);
     
+    // map root page
 	if ( self.showHome )
 	{
 		// Note: if site map IS home, it will still be shown regardless of show site map checkbox
-        if ( self.sections )
-        {
-            [[context HTMLWriter] startElement:@"h3" attributes:nil];
-            [[context HTMLWriter] endElement];
-        }
-        else
-        {
-            [[context HTMLWriter] startElement:@"p" attributes:nil];
-            [[context HTMLWriter] endElement];
-        }
-
-		if (rootPage == thisPage)	// not likely but maybe possible
-		{
-			NSString *title = [rootPage title];
-            if ( title )
-            {
-                [[context HTMLWriter] writeText:title];
-            }
-		}
-		else
-		{
-            NSString *path = [context relativeURLStringOfPage:rootPage];
-            if (!path) path = @"";  // Happens for a site with no -siteURL set yet
-            
-
-            [[context HTMLWriter] startAnchorElementWithHref:path 
-                                                        title:[thisPage title]
-                                                       target:nil
-                                                          rel:nil];
-            [[context HTMLWriter] writeText:[thisPage title]];
-            [[context HTMLWriter] endElement];
-		}
-                
-        if ( self.sections )
-        {
-            [[context HTMLWriter] startElement:@"h3" attributes:nil];
-            [[context HTMLWriter] endElement];
-        }
-        else
-        {
-            [[context HTMLWriter] startElement:@"p" attributes:nil];
-            [[context HTMLWriter] endElement];
-        }
         
+        [[context HTMLWriter] startElement:(self.sections ? @"h3" : @"p") attributes:nil];
+        [self writeLinkOfPage:rootPage relativeToPage:thisPage toContext:context];
+        [[context HTMLWriter] endElement];
+                
         // observe root's observable keypaths
         id<NSFastEnumeration> keyPaths = [rootPage automaticRearrangementKeyPaths];
         for ( NSString *keyPath in keyPaths )
         {
             //FIXME: 75490: replace NOT watching title of thisPage with a DOM controller
             if ( [thisPage isEqual:rootPage] && [keyPath isEqualToString:@"title"] ) continue;
-            //FIXME: should casting be necessary? or fix the protocol?
             [(SVHTMLContext *)context addDependencyOnObject:rootPage keyPath:keyPath];
         }
 	}
     
-    if ( childPages.count > 0 )
+    // recursively map each top-level page        
+    NSArray *topLevelPages = [rootPage childPages];
+    if ( topLevelPages.count > 0 )
     {
-        if (!self.sections)
-        {
-            [[context HTMLWriter] startElement:@"ul" attributes:nil];
-        }
+        if ( !self.sections ) [[context HTMLWriter] startElement:@"ul" attributes:nil];
         
-        for ( id<SVPage> topLevelPage in childPages )
+        for ( id<SVPage> page in topLevelPages )
         {
-            // recursively add each page        
-            [self writeMapOfPage:topLevelPage
+            [self writeMapOfPage:page
+                  relativeToPage:thisPage
                        toContext:context
-                    wantsCompact:self.compact
-                    isTopSection:self.sections];
-            
-            // observe each page's observable keypaths
-            id<NSFastEnumeration> keyPaths = [topLevelPage automaticRearrangementKeyPaths];
-            for ( NSString *keyPath in keyPaths )
-            {
-                //FIXME: 75490: replace NOT watching title of thisPage with a DOM controller
-                if ( [topLevelPage isEqual:thisPage] && [keyPath isEqualToString:@"title"] ) continue;
-                //FIXME: should casting be necessary? or fix the protocol?
-                [(SVHTMLContext *)context addDependencyOnObject:topLevelPage keyPath:keyPath];
-            }
+                       asSection:self.sections
+                    wantsCompact:self.compact];
         }
         
-        if (!self.sections)
-        {
-            [[context HTMLWriter] endElement];
-        }
+        if ( !self.sections ) [[context HTMLWriter] endElement];
     }
 }
+
 
 #pragma mark -
 #pragma mark Properties
