9ae69db2da0a327a2a74b5a0fa0a3cbcf71a1cf4
Mike Abdullah <mabdullah@karelia.com>
2010-06-02 16:40:44 +0000

    Make the *text* element have no outline.

--- App/SVRichTextDOMController.m
+++ App/SVRichTextDOMController.m
@@ -225,7 +225,7 @@ static NSString *sBodyTextObservationContext = @"SVBodyTextObservationContext";
     [super webEditorTextDidBeginEditing];
     
     // A bit crude, but we don't want WebKit's usual focus ring
-    [[[self HTMLElement] style] setProperty:@"outline" value:@"none" priority:@""];
+    [[[self textHTMLElement] style] setProperty:@"outline" value:@"none" priority:@""];
 }
 
 #pragma mark Responding to Changes
