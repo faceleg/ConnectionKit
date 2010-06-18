01abd26fec334bfef5ca4fdb82d2d89e13873298
Mike Abdullah <mabdullah@karelia.com>
2010-06-18 14:20:33 +0000

    Better docs.

--- App/SVSiteItemViewController.h
+++ App/SVSiteItemViewController.h
@@ -14,7 +14,14 @@
 
 @protocol SVSiteItemViewController
 
-// If your controller is not ready yet, return NO. Call -setSelectedViewController:self on the Web Content Area Controller when done
+/*!
+ *  Called by the Web Content Area Controller when:
+ *  A)  The user changes view type
+ *  B)  The selected pages change
+ *
+ *  So this is a good point to update your view to reflect the selection.
+ *  If the view is not ready yet (perhaps it's an asynchronous update process), return NO. Then call -setSelectedViewController:self on controller when done.
+ */
 - (BOOL)viewShouldAppear:(BOOL)animated
 webContentAreaController:(SVWebContentAreaController *)controller;
 
