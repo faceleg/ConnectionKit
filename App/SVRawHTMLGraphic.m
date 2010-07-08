6a3cce4a5fa65e32ff33b6903ca696cae21a8595
Mike Abdullah <mabdullah@karelia.com>
2010-07-08 11:39:55 +0000

    Generating a nice little placeholder for non-preview HTML. #79279

--- App/SVRawHTMLGraphic.m
+++ App/SVRawHTMLGraphic.m
@@ -8,6 +8,8 @@
 
 #import "SVRawHTMLGraphic.h"
 
+#import "SVHTMLContext.h"
+
 
 @implementation SVRawHTMLGraphic 
 
@@ -16,16 +18,22 @@
 @dynamic lastValidMarkupDigest;
 @dynamic shouldPreviewWhenEditing;
 
-@class SVWebEditorViewController;
-
 #pragma mark HTML
 
 - (void)writeBody:(SVHTMLContext *)context;
 {
-    [context writeHTMLString:[self HTMLString]];
+    // Usually, just write out the code and be done
+    if (![[self shouldPreviewWhenEditing] boolValue] && ![context shouldWriteServerSideScripts])
+    {
+        [context writeHTMLString:@"<span style=\"background:rgb(0,127,255); -webkit-border-radius:3px; padding:2px 5px; color:white; font-size:80%;\">HTML</span>"];
+    }
+    else
+    {
+        [context writeHTMLString:[self HTMLString]];
+        [context addDependencyOnObject:self keyPath:@"HTMLString"];
+    }
 	
 	// Changes to any of these properties will be a visible change
-    [context addDependencyOnObject:self keyPath:@"HTMLString"];
     [context addDependencyOnObject:self keyPath:@"docType"];
     [context addDependencyOnObject:self keyPath:@"shouldPreviewWhenEditing"];
 }
