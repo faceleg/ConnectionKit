83820a48279333d7ae2dfe134f4f8521ae92b955
Mike Abdullah <mabdullah@karelia.com>
2010-07-16 10:33:09 +0000

    Hide docType controls from plug-in developers.

--- App/SVPlugIn.h
+++ App/SVPlugIn.h
@@ -77,9 +77,6 @@
 - (BOOL)liveDataFeeds;
 - (BOOL)shouldWriteServerSideScripts;   // YES when -isForPublishing, but not when validating page
 
-// Call if your plug-in supports only particular HTML doc types. Otherwise, leave alone! Calling mid-write may have no immediate effect; instead the system will try another write after applying the limit.
-- (void)limitToMaxDocType:(KTDocType)docType;
-
 - (id <SVPage>)page;
 
 @end
