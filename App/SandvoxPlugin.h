928e00c7c82802c686a89c72aa545c6925b88b0b
Mike Abdullah <mabdullah@karelia.com>
2010-07-19 22:13:58 +0000

    Plug-ins don't need NSSet additions.

--- App/SandvoxPlugin.h
+++ App/SandvoxPlugin.h
@@ -57,7 +57,6 @@
 #import "NSBundle+Karelia.h"
 #import "NSData+Karelia.h"
 #import "NSObject+Karelia.h"
-//#import "NSSet+KTExtensions.h"
 #import "NSString+KTExtensions.h"
 #import "NSString+Karelia.h"
 #import "NSThread+Karelia.h"
