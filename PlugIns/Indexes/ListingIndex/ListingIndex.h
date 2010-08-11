9ae8bbf98eb722e00b0065d351d79e532951082c
Terrence Talbot <ttalbot@karelia.com>
2010-08-11 02:08:25 +0000

    oddly, to avoid assertion failure on _plugIn, I seem to need to add an empty class for the system to call
    shall we discuss?
    
    otherwise, this seems to work!

---
+++ PlugIns/Indexes/ListingIndex/ListingIndex.h
@@ -0,0 +1,17 @@
+//
+//  ListingIndex.h
+//  ListingIndex
+//
+//  Created by Terrence Talbot on 8/10/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SandvoxPlugin.h"
+
+@interface ListingIndex : SVIndexPlugIn
+{
+
+}
+
+@end
---
+++ PlugIns/Indexes/ListingIndex/ListingIndex.m
@@ -0,0 +1,14 @@
+//
+//  ListingIndex.m
+//  ListingIndex
+//
+//  Created by Terrence Talbot on 8/10/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "ListingIndex.h"
+
+
+@implementation ListingIndex
+
+@end
