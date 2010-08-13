5c6bcb8bd442815fc65d08deab2f7e1081b8d61c
Mike Abdullah <mabdullah@karelia.com>
2010-08-13 00:17:42 +0000

    Think this is what the keypath should be really.

--- App/SVTemplateParser.m
+++ App/SVTemplateParser.m
@@ -507,7 +507,7 @@ static NSString *kStringIndicator = @"'";					// [[' String to localize in curre
 	NSString *theString = [tag substringFromIndex:1];			// String to localize in TARGET language
 	
 	NSBundle *theBundle = [[self cache] valueForKeyPath:@"plugin.bundle"];
-	NSString *language = [[self cache] valueForKeyPath:@"currentPage.master.language"];
+	NSString *language = [[self cache] valueForKeyPath:@"parser.currentPage.master.language"];
 	if (!language) language = @"en";	// fallback just in case
 	NSString *theNewString = [theBundle localizedStringForString:theString language:language];
 	
