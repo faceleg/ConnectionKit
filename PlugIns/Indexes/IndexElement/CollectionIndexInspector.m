0e10349310ea619606a7fb1dc68f87005409a9c3
Terrence Talbot <ttalbot@karelia.com>
2010-08-16 17:59:38 +0000

    move IndexElement into PlugIns

---
+++ PlugIns/Indexes/IndexElement/CollectionIndexInspector.m
@@ -0,0 +1,86 @@
+//
+//  CollectionInspector.m
+//  IndexElement
+//
+//  Copyright 2006-2010 Karelia Software. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  *  Redistribution of source code must retain the above copyright notice,
+//     this list of conditions and the follow disclaimer.
+//
+//  *  Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other material provided with the distribution.
+//
+//  *  Neither the name of Karelia Software nor the names of its contributors
+//     may be used to endorse or promote products derived from this software
+//     without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS-IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUR OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Community Note: This code is distrubuted under a modified BSD License.
+//  We encourage you to share your Sandvox Plugins similarly.
+//
+
+#import "CollectionIndexInspector.h"
+#import "CollectionIndexPlugIn.h"
+
+
+@implementation CollectionIndexInspector
+
+
+- (void)awakeFromNib
+{
+    // enable target icon
+    //FIXME: remove this if KTLinkSourceView is enabled by default #84080
+    [oLinkView setEnabled:YES];
+    
+	// Connect up the target icon if needed
+	NSArray *selectedObjects = [[self inspectedObjectsController] selectedObjects];
+	id<SVPage> collection = (id<SVPage>)[NSNull null];		// placeholder for not known
+	NSCellStateValue state = NSMixedState;
+	for ( CollectionIndexPlugIn *plugIn in selectedObjects )
+	{
+		if ( (collection == (id<SVPage>)[NSNull null]) )
+		{
+			collection = plugIn.indexedCollection;	// first pass through
+			state = (nil != collection) ? NSOnState : NSOffState;
+		}
+		else
+		{
+			if ( collection != plugIn.indexedCollection )
+			{
+				state = NSMixedState;
+				break;		// no point in continuing; it's a mixed state and there's no going back
+			}
+		}
+	}
+	[oLinkView setConnected:(state == NSOnState)];
+}
+
+
+#pragma mark -
+#pragma mark KTLinkSourceViewDelegate
+
+- (void)linkSourceConnectedTo:(id<SVPage>)aPage
+{
+	if (aPage)
+	{
+		[[[self inspectedObjectsController] selection] setValue:aPage forKey:@"indexedCollection"];
+		[oLinkView setConnected:YES];
+	}
+}
+
+@end
