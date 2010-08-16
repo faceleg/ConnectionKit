3d557d17e6e80968794662f65942862fff5693a6
Terrence Talbot <ttalbot@karelia.com>
2010-08-16 18:21:46 +0000

    add inspector class, also named by convention

---
+++ PlugIns/Indexes/CollectionArchiveElement/CollectionArchiveInspector.h
@@ -0,0 +1,16 @@
+//
+//  CollectionArchiveInspector.h
+//  CollectionArchiveElement
+//
+//  Created by Terrence Talbot on 8/16/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CollectionArchiveInspector : NSObject {
+
+}
+
+@end
---
+++ PlugIns/Indexes/CollectionArchiveElement/CollectionArchiveInspector.m
@@ -0,0 +1,14 @@
+//
+//  CollectionArchiveInspector.m
+//  CollectionArchiveElement
+//
+//  Created by Terrence Talbot on 8/16/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "CollectionArchiveInspector.h"
+
+
+@implementation CollectionArchiveInspector
+
+@end
