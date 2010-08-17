1935a4a8de3e78322aaaac47b7ac73cfd46399a5
Terrence Talbot <ttalbot@karelia.com>
2010-08-17 19:04:11 +0000

    whoops, gotta add these, too!

---
+++ PlugIns/Elements/PageCounterElement/PageCounterInspector.h
@@ -0,0 +1,17 @@
+//
+//  PageCounterInspector.h
+//  PageCounterElement
+//
+//  Created by Terrence Talbot on 8/17/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "SandvoxPlugin.h"
+
+
+@interface PageCounterInspector : SVInspectorViewController 
+{
+
+}
+
+@end
---
+++ PlugIns/Elements/PageCounterElement/PageCounterInspector.m
@@ -0,0 +1,14 @@
+//
+//  PageCounterInspector.m
+//  PageCounterElement
+//
+//  Created by Terrence Talbot on 8/17/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "PageCounterInspector.h"
+
+
+@implementation PageCounterInspector
+
+@end
