30fc43a0b87922c291a8eac4b29260b2e94a5f86
Terrence Talbot <ttalbot@karelia.com>
2010-08-26 18:21:02 +0000

    add PhotoGridIndexInspector

---
+++ PlugIns/Indexes/PhotoGridIndex/PhotoGridIndexInspector.h
@@ -0,0 +1,16 @@
+//
+//  PhotoGridIndexInspector.h
+//  PhotoGridIndex
+//
+//  Created by Terrence Talbot on 8/26/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PhotoGridIndexInspector : NSObject {
+
+}
+
+@end
---
+++ PlugIns/Indexes/PhotoGridIndex/PhotoGridIndexInspector.m
@@ -0,0 +1,14 @@
+//
+//  PhotoGridIndexInspector.m
+//  PhotoGridIndex
+//
+//  Created by Terrence Talbot on 8/26/10.
+//  Copyright 2010 Terrence Talbot. All rights reserved.
+//
+
+#import "PhotoGridIndexInspector.h"
+
+
+@implementation PhotoGridIndexInspector
+
+@end
