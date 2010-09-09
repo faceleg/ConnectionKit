a88d178279b8d3a1dcca9266447539dcef69ec86
Dan Wood <dwood@karelia.com>
2010-09-09 22:30:46 +0000

    whoops

--- App/SVAudio.h
+++ App/SVAudio.h
@@ -19,7 +19,7 @@
 @property(nonatomic, copy) NSNumber *autoplay;
 @property(nonatomic, copy) NSNumber *controller;	// BOOLs
 @property(nonatomic, copy) NSNumber *loop;
-@property(nonatomic, copy) NSNumber *preload;		PreloadState
+@property(nonatomic, copy) NSNumber *preload;		// PreloadState
 
 @property(nonatomic, copy) NSString *codecType;	// Like Video, we may be able to distinguish specific compatibility levels
 
