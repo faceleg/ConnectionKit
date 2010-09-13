4097c3b8a292254c8f73e1689cf0d66af34c37da
Terrence Talbot <ttalbot@karelia.com>
2010-09-13 23:21:40 +0000

    seems like we should be using addCSSWithURL: rather than addResourceWithURL:

--- PlugIns/Indexes/RSSBadgeElement/RSSBadgePlugIn.m
+++ PlugIns/Indexes/RSSBadgeElement/RSSBadgePlugIn.m
@@ -112,7 +112,7 @@
     if (path && ![path isEqualToString:@""]) 
     {
         NSURL *cssURL = [NSURL fileURLWithPath:path];
-        [context addResourceWithURL:cssURL];
+        [context addCSSWithURL:cssURL];
     }
 }
 
