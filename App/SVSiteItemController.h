17fd7d7cb870e05275a5fb6406175fba07d7adde
Mike Abdullah <mabdullah@karelia.com>
2010-10-06 10:24:10 +0000

    Trying out custom controller that grabs thumbnail from a page. #89859

---
+++ App/SVSiteItemController.h
@@ -0,0 +1,27 @@
+//
+//  SVSiteItemController.h
+//  Sandvox
+//
+//  Created by Mike on 06/10/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "SVPagesController.h"
+#import "SVMediaProtocol.h"
+
+
+@interface SVSiteItemController : NSObjectController
+{
+  @private
+    id <SVMedia>    _thumbnail;
+    
+    NSArrayController   *_pagesController;
+}
+
+@property(nonatomic, retain) id <SVMedia> thumbnailMedia;
+
+@property(nonatomic, readonly) NSArrayController *childPagesController;
+
+@end
---
+++ App/SVSiteItemController.m
@@ -0,0 +1,42 @@
+//
+//  SVSiteItemController.m
+//  Sandvox
+//
+//  Created by Mike on 06/10/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "SVSiteItemController.h"
+
+
+@implementation SVSiteItemController
+
+- (void)dealloc;
+{
+    [_pagesController release];
+    [super dealloc];
+}
+
+#pragma mark Content
+
+- (void)setContent:(id)content;
+{
+    [self unbind:@"thumbnailMedia"];
+    
+    [super setContent:content];
+    
+    // Match thumbnail up to custom image for now
+    if (content)
+    {
+        [self bind:@"thumbnailMedia"
+          toObject:content
+       withKeyPath:@"customThumbnail"
+           options:nil];
+    }
+}
+
+@synthesize thumbnailMedia = _thumbnail;
+
+@synthesize childPagesController = _pagesController;
+
+@end
