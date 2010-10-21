a3b67f2b656ccabf3cb497fc0ba48fb31b9037b5
Dan Wood <dwood@karelia.com>
2010-10-21 20:36:59 +0000

    Trying to handle zero-video-track movies. Getting close!

--- App/SVVideo.m
+++ App/SVVideo.m
@@ -168,7 +168,10 @@
 // Allow zero height for external URL.
 - (BOOL)validateHeight:(NSNumber **)height error:(NSError **)error;
 {
-    BOOL result = (*height != nil || (![self media] && [self externalSourceURL]));
+    BOOL result = (*height != nil	// height is non-nil is one way for it to be OK
+				   || (![self media] && [self externalSourceURL])	// or, if height was nil, then if media is external
+				   || (0 == self.container.naturalHeight)				// or, if neither of those, it's *natural* height is zero (no video track)
+				   );
     
     if (!result && error)
     {
@@ -754,6 +757,27 @@
 	BOOL unknownTag = NO;	// will be set below if nothing can be generated
 	NSString *videoID = nil;
 	
+	// Just like Audio .... In case this has no height, it's probably audio.
+	// WHEN EDITING, AND NO CONTROLLER, PUT IN SOMETHING VISIBLE SO WE CAN SELECT THE GRAPHIC.
+	if (!self.controller && (0 == self.container.naturalHeight.intValue) && [context isForEditing])
+	{
+		[context pushAttribute:@"style"
+						 value:[NSString stringWithFormat:
+								@"padding:1px 1px 1px 6px; color:#888; text-overflow:ellipsis; overflow:hidden; white-space:nowrap; width:%dpx; height:%dpx;",
+								self.width, self.height]];
+		[context startElement:@"div"];
+		
+		[context pushAttribute:@"width" value:[NSNumber numberWithInt:16]];
+		[context pushAttribute:@"height" value:[[NSNumber numberWithInteger:16] stringValue]];
+		[context pushAttribute:@"src" value:[[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForImageResource:@"sound_placeholder"]] absoluteString]];
+		[context pushAttribute:@"style" value:@"vertical-align:text-bottom; padding-right:4px;"];
+		[context startElement:@"img"];
+		[context endElement];
+		
+		[context writeText:[[movieSourceURL path] lastPathComponent]];
+		[context endElement];
+	}
+	
 	// START THE TAGS
 	
 	if (quicktimeTag)
@@ -1212,6 +1236,8 @@
 			movieSize = [[aMovie attributeForKey:QTMovieCurrentSizeAttribute] sizeValue];	// last resort
 		}
 	}
+	
+	// Chances are if we got here with zero width/height, there is just no video track!
 	self.container.naturalWidth  = [NSNumber numberWithFloat:movieSize.width];
 	self.container.naturalHeight = [NSNumber numberWithFloat:movieSize.height];
 }
