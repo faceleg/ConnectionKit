7112336c92bbd3a06f93915e416809a1cdb7b123
Mike Abdullah <mabdullah@karelia.com>
2010-10-22 10:15:31 +0000

    Switch over to -scrollItemToVisible: #75130

--- App/SVWebEditorViewController.m
+++ App/SVWebEditorViewController.m
@@ -447,7 +447,7 @@ static NSString *sSelectedLinkObservationContext = @"SVWebEditorSelectedLinkObse
     // Scroll selection into view
     if ([webEditor selectedItem])
     {
-        [webEditor centerSelectionInVisibleArea:self];  // this isn't quite the right method as we don't actually want to center, but it performs well enough for now. Something like -scrollItemToVisible: could be ideal
+        [webEditor scrollItemToVisible:[webEditor selectedItem]];
     }
 }
 
