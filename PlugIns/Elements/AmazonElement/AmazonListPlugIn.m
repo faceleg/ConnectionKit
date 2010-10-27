ef0f0c7841b38d82e06a99df19bcb8b322c7f926
Mike Abdullah <mabdullah@karelia.com>
2010-10-27 18:12:37 +0000

    Thumbnail widths weren't persisted. #93294

--- PlugIns/Elements/AmazonElement/AmazonListPlugIn.m
+++ PlugIns/Elements/AmazonElement/AmazonListPlugIn.m
@@ -153,7 +153,7 @@ NSString * const APProductsOrListTabIdentifier = @"productsOrList";
 
 + (NSArray *)plugInKeys
 {
-    return [NSSet setWithObjects:@"store", @"layout", @"showProductPreviews", @"frame", @"showPrices", @"showThumbnails", @"showNewPricesOnly", @"showTitles", @"showComments", @"showCreators", @"products", @"showLinkToList", nil];
+    return [NSSet setWithObjects:@"store", @"layout", @"showProductPreviews", @"frame", @"showPrices", @"showThumbnails", @"centeredThumbnailWidths", @"showNewPricesOnly", @"showTitles", @"showComments", @"showCreators", @"products", @"showLinkToList", nil];
 }
 
 @synthesize store = _store;
