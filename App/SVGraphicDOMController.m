a17f2f78bf7cc2b89118ebea36a314aa87fda5a2
Mike Abdullah <mabdullah@karelia.com>
2010-11-01 15:27:41 +0000

    Take border & padding into account for max width. #94079

--- App/SVGraphicDOMController.m
+++ App/SVGraphicDOMController.m
@@ -343,6 +343,19 @@ static NSString *sGraphicSizeObservationContext = @"SVImageSizeObservation";
                                      pseudoElement:@""];
     
     CGFloat result = [[style width] floatValue];
+    
+    
+    // Bring back down to take into account margin/border/padding. #94079
+    DOMElement *graphic = [self graphicDOMElement];
+    if (!graphic) graphic = [self HTMLElement];
+        
+    style = [[[self HTMLElement] ownerDocument] getComputedStyle:graphic
+                                                   pseudoElement:@""];
+    
+    result -= ([[style borderLeftWidth] integerValue] + [[style paddingLeft] integerValue] +
+               [[style borderRightWidth] integerValue] + [[style paddingRight] integerValue]);
+    
+    
     return result;
 }
 
