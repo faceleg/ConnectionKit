06a5b0cd05be8fec38ddc4650ca676573e178b12
Terrence Talbot <ttalbot@karelia.com>
2010-11-17 23:27:03 +0000

    update comments with additional info about comment providers

--- App/SVCommentsWindowController.h
+++ App/SVCommentsWindowController.h
@@ -6,16 +6,16 @@
 //  Copyright 2010 Karelia Software. All rights reserved.
 //
 
-// Sandvox 2 supports three comment providers: Disqus, Intense Debate, and Echo
+// Sandvox 2 supports three comment providers: Disqus, Intense Debate, and JS-Kit/Echo
 //  any existing Haloscan or JS-Kit account information in 1.x documents should be converted to Echo
 
-// Disqus is far and away the cleanest implementation from a Sandvox User's standpoint
-// Intense Debate is nice and has a wealth of features, but installation is tedious and confusion-prone
-// Echo charges $10/month -- they'll make more from Sandvox than Karelia will...
+// Disqus is far and away the cleanest implementation from a Sandvox user's standpoint
+// Intense Debate is nice and has a wealth of features, but installation is somewhat tedious and confusion-prone
+// Echo charges minimally $10/month -- they'll make more from Sandvox than Karelia will...
 
 // Disqus <http://disqus.com>
 //  installation instructions are at <http://thedisquschecker.disqus.com/admin/sandvox/>
-//  with disqus, all information about the site is configured on disqus.com and tracked by the "shortname" of the site
+//  with Disqus, all information about the site is configured on disqus.com and tracked by the "shortname" of the site
 //  all the user needs to do to connect comments is to enter the (previously registered) site shortname and publish
 //
 //  one additional possibility is for the UI to show a "cross domain receiver URL" which is basically a URL
@@ -30,17 +30,26 @@
 //  once logged in, you can get your site account id via <http://intensedebate.com/sitekey/>
 //  the tricky part is that we need the site account id, not the site key, to make the generic install work
 
-// JS-Kit, now called Echo Live <http://aboutecho.com/>
+// JS-Kit is now called Echo Live <http://aboutecho.com/>
 //  JS-Kit requires registration of the site URL on their website
+//  JS-Kit does not appear to support more than one comment system per top-level URL path
+//  so, web.me.com/mike is supported but web.me.com/mike/site1 and web.me.com/mike/site2 are not supported
 //  installation instructions: <http://wiki.aboutecho.com/w/page/19987901/Echo%20-%20Install%20-%20A%20custom%20website>
 //
 //  All that needs to be done to activate it is to include the proper JavaScript on the page
 //  <div class="js-kit-comments" permalink=""></div><script src="http://cdn.js-kit.com/scripts/comments.js"></script>
 //
-//  we also ping a URL at publish time to associate an admin email address with the site URL
+//  we also used to ping a URL at publish time to associate an admin email address with the site URL
 //  <http://js-kit.com/api/isv/site-bind?email=ttalbot@karelia.com&site=http://example.com/site/&confirmviaemail=NO>
+//  this appears to no longer be supported, we have yet to hear back from Echo
 //
 //  customers are not impressed with the pricing changes at Echo <http://support.js-kit.com/jskit/topics/why_the_big_price_change>
+
+// Haloscan is officially dead. All Haloscan accounts were converted to Echo accounts last year.
+//  <http://wiki.aboutecho.com/w/page/19987926/HaloScan-Upgrade-FAQ?SearchFor=haloscan+upgrade&sp=1>
+
+
+// all that really happens in this window controller is that certain UI properties are bound to the master while the UI is displayed
 
 #import <Cocoa/Cocoa.h>
 @class KTMaster;
