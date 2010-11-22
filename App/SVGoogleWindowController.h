a98ca0ac3e75fb72d61d4303daf664ececcfc7db
Terrence Talbot <ttalbot@karelia.com>
2010-11-22 22:37:31 +0000

    add an object controller to handle binding to site while sheet is displayed

--- App/SVGoogleWindowController.h
+++ App/SVGoogleWindowController.h
@@ -6,15 +6,21 @@
 //  Copyright 2010 Karelia Software. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+// all that really happens in this window controller is that certain UI properties are bound to the site while the UI is displayed
 
+#import <Cocoa/Cocoa.h>
+@class KTSite;
 
 @interface SVGoogleWindowController : NSWindowController 
 {
-
+    NSObjectController *_objectController;
 }
+
+@property (nonatomic, retain) IBOutlet NSObjectController *objectController;
 
 - (void)configureGoogle:(NSWindowController *)sender;
 - (IBAction)closeSheet:(id)sender;
+
+- (void)setSite:(KTSite *)master;
 
 @end
--- App/SVGoogleWindowController.m
+++ App/SVGoogleWindowController.m
@@ -8,13 +8,29 @@
 
 #import "SVGoogleWindowController.h"
 #import "Debug.h"
+#import "KTSite.h"
+
 
 @implementation SVGoogleWindowController
 
+@synthesize objectController = _objectController;
+
+- (void)dealloc
+{
+    self.objectController = nil;
+    [super dealloc];
+}
+
+- (void)setSite:(KTSite *)site
+{
+    if ( [self window] )
+    {
+        [self.objectController setContent:site];
+    }
+}
+
 - (void)configureGoogle:(NSWindowController *)sender;
 {
-    LOG((@"...configure Google..."));
-    
     [NSApp beginSheet:[self window] 
        modalForWindow:[sender window] 
         modalDelegate:self 
@@ -24,7 +40,12 @@
 
 - (void)sheetDidEnd:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo;
 {
+    if ( ![sheet makeFirstResponder:sheet] )
+    {
+        [sheet endEditingFor:nil];
+    }    
     [[self window] orderOut:nil];
+    [self.objectController setContent:nil];
 }
 
 - (IBAction)closeSheet:(id)sender
