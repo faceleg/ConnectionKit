dfd17ba0a1eb236e37f3eec0c385cda54855f828
Dan Wood <dwood@karelia.com>
2010-12-14 17:46:24 +0000

    Starting to refactor truncation slider so that it can be used in both general index and RSS inspector

---
+++ App/SVTruncationController.h
@@ -0,0 +1,18 @@
+//
+//  SVTruncationController.h
+//  Sandvox
+//
+//  Created by Dan Wood on 12/14/10.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SVTruncationController : NSObject {
+
+	IBOutlet NSSlider					*oTruncateSlider;
+
+}
+
+@end
---
+++ App/SVTruncationController.m
@@ -0,0 +1,14 @@
+//
+//  SVTruncationController.m
+//  Sandvox
+//
+//  Created by Dan Wood on 12/14/10.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "SVTruncationController.h"
+
+
+@implementation SVTruncationController
+
+@end
