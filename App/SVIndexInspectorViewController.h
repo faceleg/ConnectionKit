7f39e2f5e3a3b131c137218b727e7c95ad5c9757
Terrence Talbot <ttalbot@karelia.com>
2011-01-03 23:25:46 +0000

    update copyright holder, year

--- App/SVIndexInspectorViewController.h
+++ App/SVIndexInspectorViewController.h
@@ -3,7 +3,7 @@
 //  Sandvox
 //
 //  Created by Terrence Talbot on 8/31/10.
-//  Copyright 2010 Terrence Talbot. All rights reserved.
+//  Copyright 2011 Karelia Software. All rights reserved.
 //
 
 #import "SVInspectorViewController.h"
