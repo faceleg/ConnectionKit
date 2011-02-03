16dc873c8772267164d27845b76ab49ab57340a0
Mike Abdullah <mabdullah@karelia.com>
2011-02-03 15:14:12 +0000

    Only show collection options when a collection is selected! #104216

--- App/SVPageInspector.m
+++ App/SVPageInspector.m
@@ -216,13 +216,16 @@
     
     
     // First & Last child
-    [menu addItem:[NSMenuItem separatorItem]];
-    [menu addItemWithTitle:NSLocalizedString(@"First Child Page", "menu item")
-                    action:nil
-             keyEquivalent:@""];
-    [menu addItemWithTitle:NSLocalizedString(@"Last Child Page", "menu item")
-                    action:nil
-             keyEquivalent:@""];
+    if ([page isCollection])
+    {
+        [menu addItem:[NSMenuItem separatorItem]];
+        [menu addItemWithTitle:NSLocalizedString(@"First Child Page", "menu item")
+                        action:nil
+                 keyEquivalent:@""];
+        [menu addItemWithTitle:NSLocalizedString(@"Last Child Page", "menu item")
+                        action:nil
+                 keyEquivalent:@""];
+    }
 }
 
 - (IBAction)pickThumbnailFromPage:(NSPopUpButton *)sender;
