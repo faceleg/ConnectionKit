f3035cf5ca622d337d278971b87849da6bc4519a
Mike Abdullah <mabdullah@karelia.com>
2011-02-08 15:24:18 +0000

    Specialist context for divining thumbnail media. #104216

---
+++ App/SVPageThumbnailHTMLContext.h
@@ -0,0 +1,37 @@
+//
+//  SVPageThumbnailHTMLContext.h
+//  Sandvox
+//
+//  Created by Mike on 26/01/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+//  Specialised context that the Page Inspector uses to figure out thumbnail that's in use. That way it can follow the exact same logic as HTML generation.
+
+
+#import "SVHTMLContext.h"
+
+
+@class KSObjectKeyPathPair;
+@protocol SVPageThumbnailHTMLContextDelegate;
+
+
+@interface SVPageThumbnailHTMLContext : SVHTMLContext
+{
+  @private
+    id <SVPageThumbnailHTMLContextDelegate> _delegate;
+}
+
+@property(nonatomic, assign) id <SVPageThumbnailHTMLContextDelegate> delegate;
+@end
+
+
+@protocol SVPageThumbnailHTMLContextDelegate
+
+- (void)pageThumbnailHTMLContext:(SVPageThumbnailHTMLContext *)context
+              didAddMediaWithURL:(NSURL *)mediaURL;
+
+- (void)pageThumbnailHTMLContext:(SVPageThumbnailHTMLContext *)context
+                   addDependency:(KSObjectKeyPathPair *)dependency;
+
+@end
---
+++ App/SVPageThumbnailHTMLContext.m
@@ -0,0 +1,35 @@
+//
+//  SVPageThumbnailHTMLContext.m
+//  Sandvox
+//
+//  Created by Mike on 26/01/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import "SVPageThumbnailHTMLContext.h"
+
+#import "KSObjectKeyPathPair.h"
+
+
+@implementation SVPageThumbnailHTMLContext
+
+@synthesize delegate = _delegate;
+
+- (NSURL *)addMedia:(id <SVMedia>)media;
+{
+    [[self delegate] pageThumbnailHTMLContext:self didAddMediaWithURL:[media mediaURL]];
+    
+    return [super addMedia:media];
+}
+
+- (void) addDependencyOnObject:(NSObject *)object keyPath:(NSString *)keyPath;
+{
+    [super addDependencyOnObject:object keyPath:keyPath];
+    
+    KSObjectKeyPathPair *pair = [[KSObjectKeyPathPair alloc] initWithObject:object keyPath:keyPath];
+    
+    [[self delegate] pageThumbnailHTMLContext:self addDependency:pair];
+    [pair release];
+}
+
+@end
