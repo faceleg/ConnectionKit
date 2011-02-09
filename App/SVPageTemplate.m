632462007a9d7f494147463b79c5f31dbdd38ba9
Dan Wood <dwood@karelia.com>
2011-02-09 17:44:46 +0000

    take off subtitles from page templates -- I forgot to check this in

--- App/SVPageTemplate.m
+++ App/SVPageTemplate.m
@@ -144,8 +144,8 @@
         [aTemplate release];
         
         aTemplate = [[SVPageTemplate alloc] init];
-        [aTemplate setTitle:NSLocalizedString(@"Empty/Text", "menu item title")];
-		[aTemplate setSubtitle:NSLocalizedString(@"Without Sidebar", "menu item subtitle")];
+        [aTemplate setTitle:NSLocalizedString(@"Empty/Text — Without Sidebar", "menu item title")];
+		// [aTemplate setSubtitle:NSLocalizedString(@"Without Sidebar", "menu item subtitle")];
 		[aTemplate setIcon:[NSImage imageNamed:@"page_empty"]];
         [aTemplate setPageProperties:[NSDictionary dictionaryWithObject:NSBOOL(NO) forKey:@"showSidebar"]];
         [buffer addObject:aTemplate];
@@ -161,8 +161,8 @@
         
         aTemplate = [[SVPageTemplate alloc] initWithGraphicFactory:aGraphicFactory];
  		[aTemplate setIcon:[NSImage imageNamed:@"page_photo"]];
-		[aTemplate setTitle:NSLocalizedString(@"Photo/Video", "menu item title")];
-  		[aTemplate setSubtitle:NSLocalizedString(@"Without Sidebar", "menu item subtitle")];
+		[aTemplate setTitle:NSLocalizedString(@"Photo/Video — Without Sidebar", "menu item title")];
+  		//[aTemplate setSubtitle:NSLocalizedString(@"Without Sidebar", "menu item subtitle")];
         [aTemplate setPageProperties:NSDICT(NSBOOL(NO), @"showSidebar", NSLocalizedString(@"Photo", "page title"), @"title")];
 		[buffer addObject:aTemplate];
         [aTemplate release];
