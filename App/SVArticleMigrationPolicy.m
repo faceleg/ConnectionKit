514020b9dc45c9d57b0ea6912021c4e33cc10515
Mike Abdullah <mabdullah@karelia.com>
2011-02-15 18:06:26 +0000

    Import article text.

---
+++ App/SVArticleMigrationPolicy.m
@@ -0,0 +1,32 @@
+//
+//  SVArticleMigrationPolicy.m
+//  Sandvox
+//
+//  Created by Mike on 15/02/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import "SVArticleMigrationPolicy.h"
+
+#import "SVArticle.h"
+#import "KSExtensibleManagedObject.h"
+
+
+@implementation SVArticleMigrationPolicy
+
+- (BOOL)createDestinationInstancesForSourceInstance:(NSManagedObject *)sInstance entityMapping:(NSEntityMapping *)mapping manager:(NSMigrationManager *)manager error:(NSError **)error;
+{
+    NSManagedObject *article = [SVArticle insertPageBodyIntoManagedObjectContext:[manager destinationContext]];
+    
+    NSDictionary *properties = [KSExtensibleManagedObject unarchiveExtensibleProperties:[sInstance valueForKey:@"extensiblePropertiesData"]];
+    
+    NSString *html = [properties objectForKey:@"richTextHTML"];
+    if (!html) html = @"<p>Non-text pages</p>";
+    [article setValue:html forKey:@"string"];
+    
+    [manager associateSourceInstance:sInstance withDestinationInstance:article forEntityMapping:mapping];
+     
+    return YES;
+}
+
+@end
