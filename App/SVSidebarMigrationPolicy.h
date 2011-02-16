78fd470af9147ddaf46ef16333d8bc28eec5f987
Mike Abdullah <mabdullah@karelia.com>
2011-02-16 12:28:11 +0000

    Stab at custom sidebar pagelet import.

---
+++ App/SVSidebarMigrationPolicy.h
@@ -0,0 +1,13 @@
+//
+//  SVSidebarMigrationPolicy.h
+//  Sandvox
+//
+//  Created by Mike on 16/02/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SVSidebarMigrationPolicy : NSEntityMigrationPolicy
+@end
