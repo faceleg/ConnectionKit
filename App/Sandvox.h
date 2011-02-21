69a395985cc716104f1ddb0c62244803c50b756e
Terrence Talbot <ttalbot@karelia.com>
2011-02-21 22:58:07 +0000

    #107835 -- expose SVLocalizedString()
    let the flames begin!

--- App/Sandvox.h
+++ App/Sandvox.h
@@ -42,3 +42,11 @@
 #import "SVPasteboardItem.h"
 #import "SVWebLocation.h"
 #import "SVURLFormatter.h"
+
+// Localization
+//  requires adding a Run Script Build Phase of
+//  cd ${SRCROOT}; genstrings -littleEndian -q -u -s SVLocalizedString -o en.lproj *.m
+#ifndef SVLocalizedString(key,comment)
+    #define SVLocalizedString(key,comment) [[NSBundle bundleForClass:[self class]] localizedStringForKey:(key) value:@"" table:nil]
+#endif
+
