2f83faa243cdd4195e82982f615232982611d4cc
Mike Abdullah <mabdullah@karelia.com>
2011-02-23 13:13:18 +0000

    Rearrange to point out some classes are for text editing.

---
+++ App/SVGraphicContainer.h
@@ -0,0 +1,15 @@
+//
+//  SVGraphicContainer.h
+//  Sandvox
+//
+//  Created by Mike on 23/02/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@protocol SVGraphicContainer
+
+
+@end
