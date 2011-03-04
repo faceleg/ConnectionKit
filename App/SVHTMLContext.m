7490142112a6843157b8fb165245736dbf36ae3c
Dan Wood <dwood@karelia.com>
2011-03-04 23:08:25 +0000

    jquery to 1.5.1 ahoy!

--- App/SVHTMLContext.m
+++ App/SVHTMLContext.m
@@ -235,7 +235,7 @@
 - (void)writeJQueryImport
 {
 	// We might want to update this if a major new stable update comes along. We'd put fresh copies in the app resources.
-#define JQUERY_VERSION @"1.5"
+#define JQUERY_VERSION @"1.5.1"
 	
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 	NSURL *jQueryURL = nil;
