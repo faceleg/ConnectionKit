b926ba7c3f8ebc771eb959d932505f7c236672b5
Dan Wood <dwood@karelia.com>
2011-04-05 19:07:59 +0000

    Migration: Put a period at the end of the sentence.  Use two separate strings for sentences so it's easier to localize.

--- App/SVMigrationDocument.m
+++ App/SVMigrationDocument.m
@@ -234,12 +234,10 @@ originalContentsURL:(NSURL *)inOriginalContentsURL
     
     
     NSString *path = [[self fileURL] path];
-    //path = [KTDataMigrator renamedFileName:path modelVersion:kKTModelVersion_ORIGINAL];
-    filename = [[NSFileManager defaultManager] displayNameAtPath:path];
     
-    message = [NSString stringWithFormat:
-               NSLocalizedString(@"Before it can be opened, this document must be upgraded to the latest Sandvox data format.\n\nA backup of the original document will be kept in the same folder in case you need to refer back to it.","document upgrade informative text"),
-               filename];
+    message = [NSString stringWithFormat:@"%@\n\n%@",
+               NSLocalizedString(@"Before it can be opened, this document must be upgraded to the latest Sandvox data format.","document upgrade informative text"),
+               NSLocalizedString(@"A backup of the original document will be kept in the same folder in case you need to refer back to it.","document upgrade informative text")];
     [informativeTextField setStringValue:message];
     
 	[cancelButton setTitle:NSLocalizedString(@"Cancel","Button title")];
@@ -255,8 +253,7 @@ originalContentsURL:(NSURL *)inOriginalContentsURL
     BOOL result = [super prepareSavePanel:savePanel];
     if (result && ![[self fileType] isEqualToString:kSVDocumentTypeName])
     {
-        NSString *message = [NSString stringWithFormat:
-                   NSLocalizedString(@"Before it can be opened, this document must be upgraded to the latest Sandvox data format","document upgrade informative text")];
+        NSString *message = NSLocalizedString(@"Before it can be opened, this document must be upgraded to the latest Sandvox data format.","document upgrade informative text");
         
         [savePanel setMessage:message];
         
