a7401ad61dff2c4170f9dce206b961edd83ca98d
Mike Abdullah <mabdullah@karelia.com>
2011-04-11 20:35:31 +0000

    Implementing -isConstrainProportionsEditable: will do nothing.

--- PlugIns/YouTubeElement/YouTubePlugIn.m
+++ PlugIns/YouTubeElement/YouTubePlugIn.m
@@ -211,11 +211,6 @@
     NSLog(@"ratio number is %@", value);
 }
 
-- (BOOL)isConstrainProportionsEditable
-{
-    return YES;
-}
-
 - (void)makeOriginalSize;
 {
     float height = 480 / [[self constrainedAspectRatio] floatValue];
