b87bd4daf357eb06134c941b4a337231a83672c5
Mike Abdullah <mabdullah@karelia.com>
2011-04-28 09:58:13 +0000

    docs

--- App/SVIndexPlugIn.h
+++ App/SVIndexPlugIn.h
@@ -24,18 +24,35 @@
     BOOL                _enableMaxItems;
 }
 
-- (void)makeOriginalSize;   // indexes use this to set their width to nil
+#pragma mark HTML
 
+// THIS IS THE REAL MEAT OF AN INDEX PLUG-IN
+// Returns an array of SVPage objects
+// 
+// OK, maybe I should ease off the capslock. As a general rule, your plug-in should loop through
+// each page in this array, and generate HTML from them. e.g. write out the title, mod date,
+// or summary. Perhaps even ask the context to generate a thumbnail
+//
+// By using this method, the system can calculate for you which pages to show. i.e. it'll filter
+// out any the user has requested ([SVPage shouldIncludeInIndexes]). And cut off the array at
+// -maxItems if appropriate
+//
+- (NSArray *)indexedPages;
+
+// Called when there's no pages to go in the index. Default implementation simply writes out a bit of explanatory text, but you can override to provide a richer placeholder instead
+- (void)writePlaceholderHTML:(id <SVPlugInContext>)context;
+
+
+#pragma mark Properties
+/*  The properties most indexes display in their inspector */
 @property(nonatomic, retain, readonly) id <SVPage> indexedCollection;
 @property(nonatomic) BOOL enableMaxItems;
 @property(nonatomic) NSUInteger maxItems;
 
-@property(nonatomic, readonly) NSArray *indexedPages;
 
-
-#pragma mark HTML
-// Called when there's no pages to go in the index. Default implementation simply writes out a bit of explanatory text, but you can override to provide a richer placeholder instead.
-- (void)writePlaceholderHTML:(id <SVPlugInContext>)context;
+#pragma mark Metrics
+// Plug-ins normally default to 200px wide. Indexes instead go for nil (auto) width
+- (void)makeOriginalSize;
 
 
 @end
