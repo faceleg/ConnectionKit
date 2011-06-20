da1fe04ff537f584f9bfbc78bc875830a502e835
Mike Abdullah <mabdullah@karelia.com>
2011-06-20 19:58:47 +0000

    Remove SVDesignChooserImageBrowserCell. It makes the Leopard grouchy.

--- App/SVDesignChooserImageBrowserView.m
+++ App/SVDesignChooserImageBrowserView.m
@@ -7,7 +7,6 @@
 //
 
 #import "SVDesignChooserImageBrowserView.h"
-#import "SVDesignChooserImageBrowserCell.h"
 #import "SVDesignBrowserViewController.h"
 #if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
 #else
@@ -27,13 +26,6 @@
 - (void) setSelectionIndexes:(NSIndexSet *) indexes byExtendingSelection:(BOOL) extendSelection;
 {
 	[super setSelectionIndexes:indexes byExtendingSelection:extendSelection];
-}
-// If the IKImageBrowserView asked for a custom cell class, then pass on the request to the library's delegate. 
-// That way the application is given a chance to customize the look of the browser...
-
-- (Class) _cellClass
-{
-	return [SVDesignChooserImageBrowserCell class];
 }
 
 - (void)_expandButtonClicked:(NSDictionary *)dict;
@@ -55,13 +47,6 @@
 
 - (void) awakeFromNib
 {
-	_cellClass = [self _cellClass];
-	
-	if ([self respondsToSelector:@selector(setCellClass:)])
-	{
-		[self performSelector:@selector(setCellClass:) withObject:_cellClass];
-	}
-	
 	[self setAllowsEmptySelection:NO];	// doesn't seem to stick when set in IB
 	
 	NSMutableParagraphStyle *pStyle = [[[NSMutableParagraphStyle alloc] init] autorelease];
@@ -102,13 +87,6 @@
 	[self setCellSize:NSMakeSize(120,100)];	// a bit wider to allow for 4 columns.  EMPIRICAL - not too small to shrink, not to big to allow > 100x65 sizes
 }
 
-
-// This method is for 10.6 only. Create and return a cell. Please note that we must not autorelease here!
-
-- (IKImageBrowserCell*) newCellForRepresentedItem:(id)inCell
-{
-	return [[_cellClass alloc] init];
-}
 
 - (void)keyDown:(NSEvent *)theEvent
 {
