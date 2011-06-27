376cdfa9cbf3300d92f9c603accf49bafc9792d3
Terrence Talbot <ttalbot@karelia.com>
2011-06-26 17:31:15 -0700

    initial add of +1 project

---
+++ PlugIns/GooglePlusOneElement/GooglePlusOnePlugIn.h
@@ -0,0 +1,18 @@
+//
+//  GooglePlusOnePlugIn.h
+//  GooglePlusOneElement
+//
+//  Created by Terrence Talbot on 6/1/11.
+//  Copyright 2011 Terrence Talbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+// http://code.google.com/apis/+1button/#target-url
+
+@interface GooglePlusOnePlugIn : NSObject {
+
+}
+
+@end
---
+++ PlugIns/GooglePlusOneElement/GooglePlusOnePlugIn.m
@@ -0,0 +1,14 @@
+//
+//  GooglePlusOnePlugIn.m
+//  GooglePlusOneElement
+//
+//  Created by Terrence Talbot on 6/1/11.
+//  Copyright 2011 Terrence Talbot. All rights reserved.
+//
+
+#import "GooglePlusOnePlugIn.h"
+
+
+@implementation GooglePlusOnePlugIn
+
+@end
