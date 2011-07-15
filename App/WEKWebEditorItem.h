4517add8e77f42596490b47a5c8bbfec65fa82e0
Mike Abdullah <me@mikeabdullah.net>
2011-07-15 17:22:10 +0100

    Web Editor Items track their web editor.

--- App/WEKWebEditorItem.h
+++ App/WEKWebEditorItem.h
@@ -40,7 +40,9 @@
 - (void)replaceChildWebEditorItem:(WEKWebEditorItem *)oldItem withItems:(NSArray *)newItems;
 - (void)removeFromParentWebEditorItem;
 
-- (void)itemWillMoveToParentWebEditorItem:(WEKWebEditorItem *)newParentItem;
+// Be sure to call super from these methods
+- (void)itemWillMoveToParentWebEditorItem:(WEKWebEditorItem *)newParentItem; 
+- (void)itemWillMoveToWebEditor:(WEKWebEditorView *)newWebEditor;
 - (void)itemDidMoveToParentWebEditorItem;
 
 - (NSEnumerator *)enumerator;
