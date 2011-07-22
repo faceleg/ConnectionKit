1d21c6a7dc8b397ccf27b1f2dbd342d3e7291236
Mike Abdullah <me@mikeabdullah.net>
2011-07-23 00:04:17 +0100

    When alignment cell is clicked, pass on to first responder.

---
+++ App/SVTextInspector.h
@@ -0,0 +1,18 @@
+//
+//  SVTextInspector.h
+//  Sandvox
+//
+//  Created by Mike on 22/07/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import "KSInspectorViewController.h"
+
+
+@interface SVTextInspector : KSInspectorViewController {
+
+}
+
+- (IBAction)changeAlignment:(NSSegmentedControl *)sender;
+
+@end
---
+++ App/SVTextInspector.m
@@ -0,0 +1,36 @@
+//
+//  SVTextInspector.m
+//  Sandvox
+//
+//  Created by Mike on 22/07/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import "SVTextInspector.h"
+
+
+@implementation SVTextInspector
+
+- (IBAction)changeAlignment:(NSSegmentedControl *)sender;
+{
+    switch ([[sender cell] tagForSegment:[sender selectedSegment]])
+    {
+        case NSLeftTextAlignment:
+            [NSApp sendAction:@selector(alignLeft:) to:nil from:self];
+            break;
+            
+        case NSCenterTextAlignment:
+            [NSApp sendAction:@selector(alignCenter:) to:nil from:self];
+            break;
+            
+        case NSRightTextAlignment:
+            [NSApp sendAction:@selector(alignRight:) to:nil from:self];
+            break;
+            
+        case NSJustifiedTextAlignment:
+            [NSApp sendAction:@selector(alignJustified:) to:nil from:self];
+            break;
+    }
+}
+
+@end
