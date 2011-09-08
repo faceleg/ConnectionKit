552b0de6cc8d5951450b5269564ebffe77846d97
Mike Abdullah <me@mikeabdullah.net>
2011-09-08 13:24:01 +0100

    Splitting out WebEditingKit for editing controller.

--- App/SVWebViewSelectionController.h
+++ App/SVWebViewSelectionController.h
@@ -6,42 +6,14 @@
 //  Copyright 2011 Karelia Software. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import "WEKEditingController.h"
 
 
 @class SVLink;
 
 
-@interface SVWebViewSelectionController : NSResponder <NSUserInterfaceValidations>
-{
-  @private
-    WebView     *_webView;
-    DOMRange    *_selection;
-}
+@interface SVWebViewSelectionController : WEKEditingController
 
-
-- (id)initWithWebView:(WebView *)webView;
-
-
-#pragma mark Strikethrough
-- (IBAction)strikethrough:(id)sender;   // oddly, WebKit doesn't supply this command
-
-
-#pragma mark Lists
-
-// 0 for non-lists, 1+ for lists, NSMultipleValuesMarker for mixtures
-@property(nonatomic, copy) NSNumber *listIndentLevel;
-@property(nonatomic, readonly) NSNumber *shallowestListIndentLevel;
-@property(nonatomic, readonly) NSNumber *deepestListIndentLevel;
-- (NSUInteger)listIndentLevelForDOMNode:(DOMNode *)node;
-
-@property(nonatomic, copy, readonly) NSNumber *listTypeTag;
-- (NSNumber *)isOrderedList;
-- (NSUInteger)listTypeTagForDOMNode:(DOMNode *)node;
-
-@property(nonatomic, retain) DOMRange *selection;
-
-- (void)insertIntoResponderChainAfterWebView:(WebView *)webView;
 
 
 #pragma mark Links
