e156c6e65498a2d27d434611b8488bc133b3cc0c
Mike Abdullah <me@mikeabdullah.net>
2011-09-16 15:35:00 +0100

    Hunting crasher #144016

---
+++ Experimental/CGImageSourceCrasher/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CGImageSourceCrasher
+//
+//  Created by Mike on 16/09/2011.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
